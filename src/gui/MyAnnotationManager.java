15d7d7ff228fe2c2cfd7c8f16291ad1fc80bb645
cbrinkro <christoph.brinkrolf@uni-bielefeld.de>
2013-07-31 16:25:33 +0000

    new Annotations (formally knows as Ranges), Shapes (Ellipse, Rectangle) works, Text-Annotations coming soon

---
+++ src/gui/MyAnnotationManager.java
@@ -0,0 +1,75 @@
+package gui;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import edu.uci.ics.jung.visualization.RenderContext;
+import edu.uci.ics.jung.visualization.annotations.Annotation;
+import edu.uci.ics.jung.visualization.annotations.AnnotationManager;
+import edu.uci.ics.jung.visualization.annotations.AnnotationPaintable;
+
+public class MyAnnotationManager extends AnnotationManager {
+
+	//private List<MyAnnotation> annotations;
+	private Annotation currentAnnotation;
+	private HashMap<Annotation, MyAnnotation> annotations; 
+	
+	
+	
+
+	public Annotation getCurrentAnnotation() {
+		return currentAnnotation;
+	}
+
+	public void setCurrentAnnotation(Annotation currentAnnotation) {
+		this.currentAnnotation = currentAnnotation;
+	}
+
+	public MyAnnotationManager(RenderContext<?, ?> rc) {
+		super(rc);
+		//this.annotations = new ArrayList<MyAnnotation>();
+		annotations = new HashMap<Annotation, MyAnnotation>();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void add(Annotation.Layer layer, Annotation<?> annotation) {
+		super.add(layer, annotation);
+		this.currentAnnotation = annotation;
+	}
+
+	public void add(Annotation.Layer layer, MyAnnotation annotation) {
+		//System.out.println("added");
+		//System.out.println(annotation.getAnnotation());
+		
+		//System.out.println(annotation.getPaint());
+		//super.add(layer, annotation);
+		//Annotation a2 = new Annotation(new Rectangle(60,60), layer, new Color(255,0,0), true, new Point2D.Double(0,0));
+		//super.add(Annotation.Layer.UPPER, a2);
+		//this.annotations.add(annotation);
+		annotations.put(annotation.getAnnotation(), annotation);
+		//System.out.println("shape: "+annotation.getShape());
+		this.currentAnnotation = null;
+		//System.out.println("anzahl: " + this.mapping.size());
+		
+	}
+
+	public void remove(Annotation<?> annotation) {
+		//this.annotations.remove(annotation);
+		annotations.remove(annotation);
+		super.remove(annotation);
+		//System.out.println("anzahl: " + this.mapping.size());
+	}
+	
+	public AnnotationPaintable getLowerAnnotationPaintable(){
+		return this.lowerAnnotationPaintable;
+	}
+	
+	public AnnotationPaintable getUpperAnnotationPaintable(){
+		return this.upperAnnotationPaintable;
+	}
+	
+	public Collection<MyAnnotation> getAnnotations(){
+		return this.annotations.values();
+	}
+
+}
