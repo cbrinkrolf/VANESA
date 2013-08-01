9ba9199c7f2576b7d4fd6dd708fb36309baaab1c
cbrinkro <christoph.brinkrolf@uni-bielefeld.de>
2013-08-01 17:15:35 +0000

    cleaned up a bit

--- src/gui/RangeSelector.java
+++ src/gui/RangeSelector.java
@@ -4,56 +4,36 @@
  */
 package gui;
 
-import database.dawis.AllElementLoader;
-import database.dawis.ForwardConnector;
-import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.VisualizationServer.Paintable;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.annotations.Annotation;
-import edu.uci.ics.jung.visualization.annotations.AnnotationManager;
 import graph.GraphInstance;
 import graph.jung.classes.MyGraph;
 import gui.images.ImagePath;
 
 import java.awt.Color;
 import java.awt.Cursor;
-import java.awt.Dimension;
-import java.awt.FontMetrics;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
-import java.awt.geom.Point2D.Double;
-import java.awt.geom.Rectangle2D;
 import java.awt.geom.RectangularShape;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import javax.swing.AbstractAction;
+
 import javax.swing.Action;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JColorChooser;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
-import javax.swing.JPopupMenu;
+
 import org.apache.commons.collections.Factory;
 import org.apache.commons.collections.map.LazyMap;
-
-import biologicalElements.GraphElementAbstract;
-import biologicalElements.Pathway;
-import biologicalObjects.nodes.BiologicalNodeAbstract;
-import biologicalObjects.nodes.CollectorNode;
-import biologicalObjects.nodes.PathwayMap;
 
 /**
  * 
@@ -103,9 +83,9 @@ public class RangeSelector extends MouseAdapter implements Paintable,
 	}
 
 	public RangeSelector() {
-		this.initShapeActions();
-		this.initColorActions();
-		this.setFillColor(fillColor);
+		//this.initShapeActions();
+		//this.initColorActions();
+		//this.setFillColor(fillColor);
 		rangeShapeEditor = new RangeShapeEditor();
 		dropRange = new JMenuItem("remove selected range");
 		dropRange.addActionListener(this);
@@ -126,6 +106,7 @@ public class RangeSelector extends MouseAdapter implements Paintable,
 	}
 
 	public void actionPerformed(ActionEvent e) {
+		System.out.println("event");
 		if (rangeShapeEditor.selected != null) {
 			Object src = e.getSource();
 			if (src == this.editRange) {
@@ -260,7 +241,7 @@ public class RangeSelector extends MouseAdapter implements Paintable,
 		return allRanges;
 	}
 
-	private Color getColor(Color oldColor) {
+	/*private Color getColor(Color oldColor) {
 		System.out.println("color: " + oldColor);
 		Color newColor = oldColor;
 		try {
@@ -271,9 +252,9 @@ public class RangeSelector extends MouseAdapter implements Paintable,
 		}
 
 		return newColor;
-	}
+	}*/
 
-	private void initColorActions() {
+	/*private void initColorActions() {
 		Action a = new AbstractAction() {
 
 			public void actionPerformed(ActionEvent e) {
@@ -290,17 +271,17 @@ public class RangeSelector extends MouseAdapter implements Paintable,
 		};
 		this.initAction(a, "font.png", "select text color");
 		this.selectColorActions.add(a);
-	}
+	}*/
 
-	private void initAction(Action a, String image, String desc) {
+	/*private void initAction(Action a, String image, String desc) {
 
 		// a.putValue(Action.SMALL_ICON, createIcon(imagePath));
 		a.putValue(Action.SMALL_ICON, new ImageIcon(imagePath.getPath(image)));
 		a.putValue(Action.SHORT_DESCRIPTION, desc);
 
-	}
+	}*/
 
-	private void initShapeActions() {
+	/*private void initShapeActions() {
 		Action a = new AbstractAction() {
 
 			public void actionPerformed(ActionEvent e) {
@@ -317,17 +298,17 @@ public class RangeSelector extends MouseAdapter implements Paintable,
 		};
 		this.initAction(a, "ellipse.png", "ellipse");
 		this.selectShapeActions.add(a);
-	}
+	}*/
 
-	private Icon createIcon(String path) {
+	/*private Icon createIcon(String path) {
 		return new ImageIcon(this.getClass().getClassLoader().getResource(path));
-	}
+	}*/
 
-	private String getInputText() {
+	/*private String getInputText() {
 		String text = JOptionPane
 				.showInputDialog("Please enter a description!");
 		return text;
-	}
+	}*/
 
 	// private Shape createTextShape(Graphics2D g2d,String text){
 	// FontRenderContext frc = g2d.getFontRenderContext();
@@ -335,18 +316,18 @@ public class RangeSelector extends MouseAdapter implements Paintable,
 	// TextLayout tl = new TextLayout(text, font, frc);
 	// return tl.getOutline(null);
 	// }
-	public int getCurrentRangeType() {
+	/*public int getCurrentRangeType() {
 		return currentRangeType;
 	}
-
-	public void setCurrentRangeType(int currentRangeType) {
+*/
+	/*public void setCurrentRangeType(int currentRangeType) {
 		this.currentRangeType = currentRangeType;
 		setEnabled(true);
 		try {
 			GraphInstance.getMyGraph().setMouseModeSelectRange();
 		} catch (NullPointerException ne) {
 		}
-	}
+	}*/
 
 	/*
 	 * @Override public void mouseDragged(MouseEvent e) { super.mouseDragged(e);
@@ -413,7 +394,7 @@ public class RangeSelector extends MouseAdapter implements Paintable,
 	 * }
 	 */
 
-	private RectangularShape createShape(Point2D p1) {
+	/*private RectangularShape createShape(Point2D p1) {
 		switch (currentRangeType) {
 		case RECTANGLE:
 			System.out.println("drin");
@@ -424,29 +405,29 @@ public class RangeSelector extends MouseAdapter implements Paintable,
 			// this.justCreated=new Line2D.Float(p1.x,p1.y,p1.x,p1.y);
 		}
 		return null;
-	}
+	}*/
 
-	private double calcYInEllipse(double x, Ellipse2D shape) {
+	/*private double calcYInEllipse(double x, Ellipse2D shape) {
 		Rectangle2D bound = shape.getBounds2D();
 		double rx = bound.getWidth() / 2, ry = bound.getHeight() / 2, centerX = bound
 				.getCenterX();
 		double coordX = x - rx;
 		double y = Math.sqrt((1 - coordX * coordX / (rx * rx)) * ry * ry);
 		return y;
-	}
+	}*/
 
-	private RectangularShape tempRect = new Rectangle2D.Double(),
-			tempEllipse = new Ellipse2D.Double();
-	private int inset = 5;
+//	private RectangularShape tempRect = new Rectangle2D.Double(),
+//			tempEllipse = new Ellipse2D.Double();
+//	private int inset = 5;
 
-	public void paint(Graphics g) {
-		if (justCreated != null) {
+	//public void paint(Graphics g) {
+		/*if (justCreated != null) {
 			Color oldColor = g.getColor();
 			g.setColor(fillColor);
 			((Graphics2D) g).draw(justCreated.shape);
 			g.setColor(oldColor);
 
-		}
+		}*/
 		/*
 		 * List<RangeInfo> shapes = this.getShapes(); for (RangeInfo info :
 		 * shapes) { if (info != null) { Color oldColor = g.getColor();
@@ -496,15 +477,15 @@ public class RangeSelector extends MouseAdapter implements Paintable,
 		 * g2d.setColor(old); // g2d.setFont(oldFont);
 		 * g2d.setTransform(oldXform);
 		 */
-	}
+	//}
 
-	public boolean useTransform() {
+	/*public boolean useTransform() {
 		return false;
-	}
+	}*/
 
-	public boolean isEnabled() {
-		return enabled;
-	}
+//	public boolean isEnabled() {
+//		return enabled;
+//	}
 
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
@@ -516,11 +497,11 @@ public class RangeSelector extends MouseAdapter implements Paintable,
 		}
 	}
 
-	public Color getFillColor() {
-		return fillColor;
-	}
+//	public Color getFillColor() {
+//		return fillColor;
+//	}
 
-	public void setFillColor(Color fillColor) {
+	/*public void setFillColor(Color fillColor) {
 		try {
 			if (!this.fillColor.equals(fillColor)) {
 				this.fillColor = new Color(fillColor.getRed(),
@@ -528,39 +509,39 @@ public class RangeSelector extends MouseAdapter implements Paintable,
 			}
 		} catch (NullPointerException e) {
 		}
-	}
+	}*/
 
-	public List<Action> getSelectShapeActions() {
+	/*public List<Action> getSelectShapeActions() {
 		return selectShapeActions;
-	}
+	}*/
 
-	public List<Action> getSelectColorActions() {
+	/*public List<Action> getSelectColorActions() {
 		return selectColorActions;
-	}
+	}*/
 
-	public Color getTextColor() {
+	/*public Color getTextColor() {
 		return textColor;
-	}
+	}*/
 
-	public void setTextColor(Color textColor) {
-		this.textColor = textColor;
-	}
+//	public void setTextColor(Color textColor) {
+//		this.textColor = textColor;
+//	}
 
-	public Color getOutlineColor() {
-		return outlineColor;
-	}
+//	public Color getOutlineColor() {
+//		return outlineColor;
+//	}
 
-	public void setOutlineColor(Color outlineColor) {
-		this.outlineColor = outlineColor;
-	}
+//	public void setOutlineColor(Color outlineColor) {
+//		this.outlineColor = outlineColor;
+//	}
 
 	public RangeShapeEditor getRangeShapeEditor() {
 		return rangeShapeEditor;
 	}
 
-	private Point2D inverseTransform(VisualizationViewer vv, Point p) {
-		return vv.getRenderContext().getMultiLayerTransformer().transform(p);// inverseTransform(p);
-	}
+//	private Point2D inverseTransform(VisualizationViewer vv, Point p) {
+//		return vv.getRenderContext().getMultiLayerTransformer().transform(p);// inverseTransform(p);
+//	}
 
 	private Point2D inverseTransform(MouseEvent e) {
 		VisualizationViewer vv = (VisualizationViewer) e.getSource();
@@ -576,25 +557,25 @@ public class RangeSelector extends MouseAdapter implements Paintable,
 		// return vv.inverseTransform(e.getPoint());
 	}
 
-	private void checkContextMenu(MouseEvent e) {
-		if (enabled && e.getButton() == MouseEvent.BUTTON3) {
-			if (rangeShapeEditor.selected != null) {
-				JPopupMenu menu = GraphInstance.getMyGraph()
-						.getVisualizationViewer().getComponentPopupMenu();
-				menu.add(editRange);
-				menu.add(dropRange);
-				menu.add(this.moveUpRange);
-				menu.add(this.moveDownRange);
-			} else {
-				JPopupMenu menu = GraphInstance.getMyGraph()
-						.getVisualizationViewer().getComponentPopupMenu();
-				menu.remove(dropRange);
-				menu.remove(editRange);
-				menu.remove(this.moveUpRange);
-				menu.remove(this.moveDownRange);
-			}
-		}
-	}
+//	private void checkContextMenu(MouseEvent e) {
+//		if (enabled && e.getButton() == MouseEvent.BUTTON3) {
+//			if (rangeShapeEditor.selected != null) {
+//				JPopupMenu menu = GraphInstance.getMyGraph()
+//						.getVisualizationViewer().getComponentPopupMenu();
+//				menu.add(editRange);
+//				menu.add(dropRange);
+//				menu.add(this.moveUpRange);
+//				menu.add(this.moveDownRange);
+//			} else {
+//				JPopupMenu menu = GraphInstance.getMyGraph()
+//						.getVisualizationViewer().getComponentPopupMenu();
+//				menu.remove(dropRange);
+//				menu.remove(editRange);
+//				menu.remove(this.moveUpRange);
+//				menu.remove(this.moveDownRange);
+//			}
+//		}
+//	}
 
 	public class RangeShapeEditor extends MouseAdapter {
 
@@ -736,5 +717,17 @@ public class RangeSelector extends MouseAdapter implements Paintable,
 				}
 			}
 		}
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean useTransform() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
