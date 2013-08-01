03877d08a9fa13109d431f2d1e249d6e1650008b
cbrinkro <christoph.brinkrolf@uni-bielefeld.de>
2013-08-01 13:10:46 +0000

    Support for Text-Annotations

--- src/gui/MyAnnotatingGraphMousePlugin.java
+++ src/gui/MyAnnotatingGraphMousePlugin.java
@@ -1,15 +1,26 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.awt.Shape;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 import java.awt.geom.RectangularShape;
 
+import javax.swing.JOptionPane;
+
 import edu.uci.ics.jung.visualization.RenderContext;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.annotations.AnnotatingGraphMousePlugin;
 import edu.uci.ics.jung.visualization.annotations.Annotation;
 import graph.GraphInstance;
 
 public class MyAnnotatingGraphMousePlugin<V, E> extends
 		AnnotatingGraphMousePlugin<V, E> {
+
+	private int currentType = AnnotationPainter.RECTANGLE;
+	private String annotationString;
 
 	public MyAnnotatingGraphMousePlugin(RenderContext<V, E> rc) {
 		super(rc);
@@ -20,18 +31,64 @@ public class MyAnnotatingGraphMousePlugin<V, E> extends
 
 	}
 
+	public void setCurrentType(int type) {
+		this.currentType = type;
+		if (type == AnnotationPainter.RECTANGLE) {
+			setRectangularShape(new Rectangle());
+		} else {
+			if (type == AnnotationPainter.ELLIPSE) {
+				setRectangularShape(new Ellipse2D.Double());
+			}
+		}
+	}
+
 	public void mouseReleased(MouseEvent e) {
-		super.mouseReleased(e);
+
+		VisualizationViewer<V, E> vv = (VisualizationViewer) e.getSource();
+		if (currentType == AnnotationPainter.TEXT) {
+			annotationString = JOptionPane.showInputDialog(vv, "Annotation:");
+			if (annotationString != null && annotationString.length() > 0) {
+				Point2D p = vv.getRenderContext().getMultiLayerTransformer()
+						.inverseTransform(down);
+				Annotation<String> annotation = new Annotation<String>(
+						annotationString, layer, new Color(0, 0, 0), false, p);
+				annotationManager.add(layer, annotation);
+			}
+		} else if (e.getModifiers() == modifiers) {
+			if (down != null) {
+				Point2D out = e.getPoint();
+				RectangularShape arect = (RectangularShape) rectangularShape
+						.clone();
+				arect.setFrameFromDiagonal(down, out);
+				Shape s = vv.getRenderContext().getMultiLayerTransformer()
+						.inverseTransform(arect);
+				Annotation<Shape> annotation = new Annotation<Shape>(s, layer,
+						annotationColor, fill, out);
+				annotationManager.add(layer, annotation);
+			}
+		}
+		down = null;
+		vv.removePostRenderPaintable(lensPaintable);
+		vv.repaint();
 
 		Annotation a = ((MyAnnotationManager) this.annotationManager)
 				.getCurrentAnnotation();
-		RectangularShape s = (RectangularShape)this.getRectangularShape().clone();
+		RectangularShape s = (RectangularShape) this.getRectangularShape()
+				.clone();
+		// System.out.println("shape: "+s);
 		if (a != null) {
-			if (s.getWidth() < 20 || s.getHeight() < 20) {
+			if (this.currentType != AnnotationPainter.TEXT
+					&& (s.getWidth() < 20 || s.getHeight() < 20)) {
 				this.annotationManager.remove(a);
 			} else {
-				MyAnnotation ma = new MyAnnotation(a, s);
-				//System.out.println(this.getRectangularShape().getWidth());
+				MyAnnotation ma;
+				if (currentType == AnnotationPainter.TEXT) {
+					ma = new MyAnnotation(a, s,
+							this.annotationString);
+				} else {
+					ma = new MyAnnotation(a, s, "");
+				}
+				// System.out.println(this.getRectangularShape().getWidth());
 				// a.setPaint(new Color(255, 255, 255));
 				/*
 				 * RangeInfo r =new RangeInfo( this.getRectangularShape(),
@@ -45,7 +102,7 @@ public class MyAnnotatingGraphMousePlugin<V, E> extends
 				// Color(255,0,0), fill, new Point2D.Double(0,0));
 				// a.setShape(this.getRectangularShape());
 				// System.out.println(a);
-				//System.out.println("addGM");
+				// System.out.println("addGM");
 				((MyAnnotationManager) this.annotationManager).add(layer, ma);
 			}
 		}
