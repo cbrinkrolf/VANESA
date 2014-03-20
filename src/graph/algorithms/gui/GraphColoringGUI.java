918fc1aecb01f9a876e32214cfb6694d55673562
mlewinsk <mlewinsk@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2014-03-20 08:01:18 +0000

    Updated Graph Coloring wit the current cluster algorithms. New Method added in InternalGraphRepresentation which allows to get the current graph Object.

--- src/graph/algorithms/gui/GraphColoringGUI.java
+++ src/graph/algorithms/gui/GraphColoringGUI.java
@@ -1,14 +1,20 @@
 package graph.algorithms.gui;
 
-
+import graph.ContainerSingelton;
+import graph.GraphContainer;
 import graph.GraphInstance;
 import graph.algorithms.NetworkProperties;
+import graph.jung.classes.MyGraph;
+import gui.MainWindowSingelton;
 import gui.images.ImagePath;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
@@ -16,12 +22,16 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+
+import cluster.ClusterComputeClient;
 
 import net.infonode.tabbedpanel.titledtab.TitledTab;
 import net.miginfocom.swing.MigLayout;
 import biologicalElements.GraphElementAbstract;
+import biologicalElements.Pathway;
 import biologicalObjects.nodes.BiologicalNodeAbstract;
 
 public class GraphColoringGUI implements ActionListener {
@@ -33,37 +43,45 @@ public class GraphColoringGUI implements ActionListener {
 
 	private JComboBox<String> chooseAlgorithm;
 	private JButton colorizebutton;
+	private JButton resetcolorbutton;
 
-	private String[] algorithmNames = { "Node Degree", "Neighbor Degree", "Clique" };
+	private String[] algorithmNames = { "Node Degree", "Neighbor Degree",
+			"Cycles (cluster)", "Cliques (cluster)", "Paths (cluster)" };
 	private int currentalgorithmindex = 0;
-	private String[] colorrangenames = {"bluesea","skyline","darkmiddle","darkleftmiddle","rainbow"};
-//	private String[] iconpaths = {"pictures/icon_colorrange_bluesea.png","pictures/icon_colorrange_skyline.png",
-//			"pictures/icon_colorrange_darkmiddle.png","pictures/icon_colorrange_dark.png","pictures/icon_colorrange_rainbow.png"};
+	private String[] colorrangenames = { "bluesea", "skyline", "darkmiddle",
+			"darkleftmiddle", "rainbow" };
+	private final int NODE_DEGREE = 0, NEIGHBOR_DEGREE = 1, CYCLES = 2,
+			CLIQUES = 3, PATHRATING = 4;
+
 	private ImageIcon[] icons;
-	
+
 	private ImageIcon currentimage;
 	private int currentimageid;
 	private JRadioButton[] colorrangeradio = new JRadioButton[colorrangenames.length];
 	private GraphColorizer gc;
 	private JCheckBox logview;
 	private ButtonGroup bg;
-	
-	//coloring variables
-	private NetworkProperties c;	
+
+	// coloring variables
+	private NetworkProperties np;
 	private Iterator<BiologicalNodeAbstract> itn;
-	private Hashtable<BiologicalNodeAbstract,Double> coloring;
+	private Hashtable<BiologicalNodeAbstract, Double> coloring;
 	private BiologicalNodeAbstract bna;
-	
+
 	private TitledTab tab;
-	
+
+	private final Color greynodecolor = new Color(-4144960);
+
 	public GraphColoringGUI() {
-		//set icon paths
+		// set icon paths
 		ImagePath imagepath = ImagePath.getInstance();
-		ImageIcon[] tmpset =  {new ImageIcon(imagepath.getPath("icon_colorrange_bluesea.png")),
+		ImageIcon[] tmpset = {
+				new ImageIcon(imagepath.getPath("icon_colorrange_bluesea.png")),
 				new ImageIcon(imagepath.getPath("icon_colorrange_skyline.png")),
-				new ImageIcon(imagepath.getPath("icon_colorrange_darkmiddle.png")),
+				new ImageIcon(
+						imagepath.getPath("icon_colorrange_darkmiddle.png")),
 				new ImageIcon(imagepath.getPath("icon_colorrange_dark.png")),
-				new ImageIcon(imagepath.getPath("icon_colorrange_rainbow.png"))};
+				new ImageIcon(imagepath.getPath("icon_colorrange_rainbow.png")) };
 		icons = tmpset;
 		currentimage = icons[0];
 		currentimageid = 0;
@@ -77,68 +95,136 @@ public class GraphColoringGUI implements ActionListener {
 		chooseAlgorithm = new JComboBox<String>(algorithmNames);
 		chooseAlgorithm.setActionCommand("algorithm");
 		chooseAlgorithm.addActionListener(this);
-	
+
 		for (int i = 0; i < colorrangenames.length; i++) {
 			colorrangeradio[i] = new JRadioButton(colorrangenames[i]);
-			colorrangeradio[i].setActionCommand(""+i);
+			colorrangeradio[i].setActionCommand("" + i);
 			colorrangeradio[i].addActionListener(this);
 			bg.add(colorrangeradio[i]);
-			
-		}				
+
+		}
 		colorizebutton = new JButton("color selection");
 		colorizebutton.setActionCommand("colorize");
 		colorizebutton.addActionListener(this);
 		colorizebutton.setEnabled(false);
 
+		resetcolorbutton = new JButton("reset colors");
+		resetcolorbutton.setActionCommand("resetcolors");
+		resetcolorbutton.addActionListener(this);
+		resetcolorbutton.setEnabled(false);
+
 		MigLayout layout = new MigLayout("", "[][grow]", "");
-		p.setLayout(layout);	
+		p.setLayout(layout);
 		p.add(new JLabel("Algorithm"), "wrap");
 		p.add(chooseAlgorithm, "wrap");
-		p.add(new JLabel("Color Range"),"wrap");
+		p.add(new JLabel("Color Range"), "wrap");
 		for (int i = 0; i < colorrangenames.length; i++) {
 			p.add(colorrangeradio[i]);
-			p.add(new JLabel(icons[i]),"wrap");
+			p.add(new JLabel(icons[i]), "wrap");
 		}
-		
+
 		logview = new JCheckBox("Data in log(10)");
 		logview.setSelected(false);
 		logview.setActionCommand("logview");
 		logview.addActionListener(this);
 		logview.setEnabled(false);
-		
-		p.add(logview);
-		p.add(colorizebutton, "span 2,align right, wrap");
-		
+
+		p.add(logview, "align left");
+		p.add(colorizebutton, "align right, wrap");
+		p.add(resetcolorbutton, "align left, wrap");
+
 	}
 
 	public void recolorGraph() {
 
-		c = new NetworkProperties();	
-		itn = c.getPathway().getAllNodes().iterator();
-		coloring = new Hashtable<BiologicalNodeAbstract,Double>();
-		
-		if (currentalgorithmindex == 0) { //node degree
-			while (itn.hasNext()) {			
+		np = new NetworkProperties();
+		itn = np.getPathway().getAllNodes().iterator();
+		coloring = new Hashtable<BiologicalNodeAbstract, Double>();
+
+		switch (currentalgorithmindex) {
+		case NODE_DEGREE:
+			while (itn.hasNext()) {
 				bna = itn.next();
-				coloring.put(bna,(double)c.getNodeDegree(c.getNodeAssignment(bna)));
-			}			
-		}else if(currentalgorithmindex == 1) {//neighbor degree
-			coloring = c.averageNeighbourDegreeTable();
-						
-		}else if(currentalgorithmindex == 2) {//cliques
-			while (itn.hasNext()) {			
-				bna = itn.next();
-			}			
-		}		
-		
-		
-		gc = new GraphColorizer(coloring, currentimageid, logview.isSelected());	
-		//recolor button enable after first Coloring, logview enabled
+				coloring.put(bna,
+						(double) np.getNodeDegree(np.getNodeAssignment(bna)));
+			}
+			break;
+		case NEIGHBOR_DEGREE:
+			coloring = np.averageNeighbourDegreeTable();
+			break;
+		case CYCLES:
+			// compute values over RMI
+			ClusterComputeClient rmicycles = new ClusterComputeClient(
+					ClusterComputeClient.CYCLE_JOB_OCCURRENCE);
+			rmicycles.setAdjMatrix(np.getAdjacencyMatrix());
+			if (rmicycles.start()) {
+				Hashtable<Integer, Double> cycledata = rmicycles
+						.getResultTable();
+
+				if (!cycledata.isEmpty()) {
+					// Map ids to BNAs
+					Iterator<Entry<Integer, Double>> it = cycledata.entrySet()
+							.iterator();
+					int key;
+					double value;
+
+					while (it.hasNext()) {
+						Entry<Integer, Double> entry = it.next();
+						key = entry.getKey();
+						value = entry.getValue();
+						// debug
+						// System.out.println(key + " " + value);
+						coloring.put(np.getNodeAssignmentbackwards(key), value);
+					}
+				}
+			}
+
+			break;
+		case CLIQUES:
+			// compute values over RMI
+			ClusterComputeClient rmicliques = new ClusterComputeClient(
+					ClusterComputeClient.CLIQUE_JOB_OCCURRENCE);
+			rmicliques.setAdjMatrix(np.getAdjacencyMatrix());
+			if (rmicliques.start()) {
+				Hashtable<Integer, Double> cliquedata = rmicliques
+						.getResultTable();
+
+				if (!cliquedata.isEmpty()) {
+					// Map ids to BNAs
+					Iterator<Entry<Integer, Double>> it = cliquedata.entrySet()
+							.iterator();
+					int key;
+					double value;
+
+					while (it.hasNext()) {
+						Entry<Integer, Double> entry = it.next();
+						key = entry.getKey();
+						value = entry.getValue();
+						// debug
+						System.out.println(key + " " + value);
+						coloring.put(np.getNodeAssignmentbackwards(key), value);
+					}
+				}
+			}
+
+			break;
+		case PATHRATING:
+			ClusterComputeClient rmiapsp = new ClusterComputeClient(
+					ClusterComputeClient.APSP_JOB);
+			rmiapsp.start();
+			break;
+
+		default:
+			break;
+		}
+
+		gc = new GraphColorizer(coloring, currentimageid, logview.isSelected());
+		// recolor button enable after first Coloring, logview enabled
 		colorizebutton.setEnabled(true);
 		logview.setEnabled(true);
+		resetcolorbutton.setEnabled(true);
 	}
 
-	
 	public void revalidateView() {
 
 		graphInstance = new GraphInstance();
@@ -153,7 +239,7 @@ public class GraphColoringGUI implements ActionListener {
 			updateWindow();
 			p.repaint();
 			p.revalidate();
-		 p.setVisible(true);
+			p.setVisible(true);
 
 		}
 	}
@@ -177,38 +263,48 @@ public class GraphColoringGUI implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 
 		String command = e.getActionCommand();
-	
+
 		if ("colorize".equals(command)) {
 			recolorGraph();
 			GraphInstance.getMyGraph().getVisualizationViewer().repaint();
-		}
+		} else if ("resetcolors".equals(command)) {
+			GraphContainer con = ContainerSingelton.getInstance();
+			Iterator<BiologicalNodeAbstract> it = (Iterator<BiologicalNodeAbstract>) con
+					.getPathway(
+							MainWindowSingelton.getInstance()
+									.getCurrentPathway()).getGraph()
+					.getAllVertices().iterator();
 
-		else if ("algorithm".equals(command)){
+			while (it.hasNext()) {
+				bna = it.next();
+				bna.setColor(greynodecolor);
+			}
+
+			GraphInstance.getMyGraph().getVisualizationViewer().repaint();
+
+		} else if ("algorithm".equals(command)) {
 			currentalgorithmindex = chooseAlgorithm.getSelectedIndex();
 			for (int i = 0; i < colorrangeradio.length; i++) {
 				if (colorrangeradio[i].isSelected()) {
 					recolorGraph();
 					break;
 				}
-			}			
+			}
 			GraphInstance.getMyGraph().getVisualizationViewer().repaint();
-		}
-		
-		else if("logview".equals(command)){
+		} else if ("logview".equals(command)) {
 			recolorGraph();
 			GraphInstance.getMyGraph().getVisualizationViewer().repaint();
 		}
-		//get proper icon path
+		// get proper icon path
 		for (int i = 0; i < colorrangenames.length; i++) {
-			if ((i+"").equals(command)){
-				currentimage=icons[i];
+			if ((i + "").equals(command)) {
+				currentimage = icons[i];
 				currentimageid = i;
 				recolorGraph();
-				//repaint, damit Farben auch angezeigt werden
-					GraphInstance.getMyGraph().getVisualizationViewer().repaint();			
+				// repaint, damit Farben auch angezeigt werden
+				GraphInstance.getMyGraph().getVisualizationViewer().repaint();
 				break;
 			}
-			
 		}
 	}
 }
