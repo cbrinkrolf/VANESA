1d2044fe2c975925dd863f5720e9ae0a7cf83cd7
toptoptop <toptoptop@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2014-04-30 11:24:56 +0000

    For Lennart: MenuBar resizable without limits.
    Some small changes.

--- src/gui/eventhandlers/ToolBarListener.java
+++ src/gui/eventhandlers/ToolBarListener.java
@@ -316,20 +316,25 @@ public class ToolBarListener implements ActionListener {
 				}
 		}
 		else if ("enterNode".equals(event)){
-//			if(graphInstance.getMyGraph() != null){
-//					w.updateHierarchyView();
-//			}else{
-//				System.out.println("No Graph exists!");
-//				}
-			MainWindowSingelton.getInstance().addView();
-		}
-		else if("mergeSelectedNodes".equals(event)){
 			if(graphInstance.getMyGraph() != null){
-			System.out.println("merge");
-			graphInstance.getPathway().mergeNodes(graphInstance.getPathway().getGraph().getVisualizationViewer().getPickedVertexState().getPicked());
+					w.updateHierarchyView();
+//					for(BiologicalNodeAbstract node : graphInstance.getPathway().getGraph().
+//							getVisualizationViewer().getPickedVertexState().getPicked()){
+//						node.getGraph().changeToGEMLayout();
+//					}
+					
 			}else{
 				System.out.println("No Graph exists!");
-			}
+				}
+		}
+		else if("mergeSelectedNodes".equals(event)){
+//			if(graphInstance.getMyGraph() != null){
+//			System.out.println("merge");
+//			graphInstance.getPathway().mergeNodes(graphInstance.getPathway().getGraph().getVisualizationViewer().getPickedVertexState().getPicked());
+//			}else{
+//				System.out.println("No Graph exists!");
+//			}
+			MainWindowSingelton.getInstance().addView();
 		}
 	}
 }
