1844e89de7b9bc62d9423fbaf6a44e107a379e80
mlewinsk <mlewinsk@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2014-07-08 14:10:03 +0000

    Parallel FR Layout on cluster (Skeleton) Batch Mapping for the cluster was added (needed when uploading multiple mappings at the same time)

---
+++ src/cluster/LayoutPoint2D.java
@@ -0,0 +1,64 @@
+package cluster;
+
+import java.io.Serializable;
+
+/**
+ * Decorator for Coordinates, 
+ * 
+ * @author mlewinsk
+ * July 2014
+ */
+
+public class LayoutPoint2D implements Serializable{
+
+	private static final long serialVersionUID = 8346840878281856405L;
+	private float x = 0.0f;
+	private float y = 0.0f;
+	
+	public LayoutPoint2D(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Float.floatToIntBits(x);
+		result = prime * result + Float.floatToIntBits(y);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LayoutPoint2D other = (LayoutPoint2D) obj;
+		if (Float.floatToIntBits(x) != Float.floatToIntBits(other.x))
+			return false;
+		if (Float.floatToIntBits(y) != Float.floatToIntBits(other.y))
+			return false;
+		return true;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+}
