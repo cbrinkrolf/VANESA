71381d0354ca61d1edb6ff2878729c5765aeefd0
toptoptop <toptoptop@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2014-07-14 16:45:01 +0000

    Edge delete dialog appears if hierarchical edge is selected for deletion (raw version).

---
+++ src/graph/gui/EdgeDeleteDialog.java
@@ -0,0 +1,116 @@
+/**
+ * 
+ */
+package graph.gui;
+
+import graph.GraphInstance;
+
+import java.util.HashMap;
+import javax.swing.ButtonGroup;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JSeparator;
+import net.miginfocom.swing.MigLayout;
+
+import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
+
+import biologicalElements.Pathway;
+import biologicalObjects.edges.BiologicalEdgeAbstract;
+
+/**
+ * @author tloka
+ * 
+ */
+public class EdgeDeleteDialog extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	JPanel panel;
+	JOptionPane pane;
+	JRadioButton selectedEdge, allEdges;
+
+	GraphInstance graphInstance = new GraphInstance();
+	Pathway pw = graphInstance.getPathway();
+	JComboBox<String> edges = new JComboBox<String>();
+	HashMap<Integer,BiologicalEdgeAbstract> edgeMap = new HashMap<Integer, BiologicalEdgeAbstract>();
+
+	/**
+	 * 
+	 */
+	public EdgeDeleteDialog(BiologicalEdgeAbstract edge) {
+
+		edgeMap.put(-1, edge);
+		//Container contentPane = getContentPane();
+		MigLayout layout = new MigLayout("", "[left]");
+
+		ButtonGroup group = new ButtonGroup();
+		selectedEdge = new JRadioButton("Selected Edge");
+		allEdges = new JRadioButton("All Sub-Edges");
+		selectedEdge.setSelected(true);
+
+		group.add(selectedEdge);
+		group.add(allEdges);
+
+		panel = new JPanel(layout);
+		
+		panel.add(new JLabel("Select Start Node"), "");
+		addAllConnectingEdges(edge, edges, edgeMap);
+		AutoCompleteDecorator.decorate(edges);
+		panel.add(edges, "span,wrap,growx,gap 10");
+		
+		// panel.add(new JSeparator(), "span, growx, wrap 10");
+
+		panel.add(new JSeparator(), "span, growx, wrap 10, gaptop 7 ");
+		
+		panel.add(new JLabel("Edge(s) to delete:"), "");
+		panel.add(selectedEdge, "gap 10");
+		panel.add(allEdges, "span,wrap,growx");
+
+		pane = new JOptionPane(panel, JOptionPane.PLAIN_MESSAGE,
+				JOptionPane.OK_CANCEL_OPTION);
+	}
+	
+	private void addAllConnectingEdges(BiologicalEdgeAbstract edge, JComboBox<String> edgeBox, HashMap<Integer, BiologicalEdgeAbstract> map){
+		for(BiologicalEdgeAbstract childEdge : edge.getTo().getConnectingEdges()){
+			if(childEdge.getFrom().getCurrentShownParentNode(graphInstance.getPathway().getGraph())==edge.getFrom()){
+				if(childEdge.isDirected()){
+					edgeBox.addItem(childEdge.getFrom().getLabel() + " |-> " + childEdge.getTo().getLabel());
+				} else {
+					edgeBox.addItem(childEdge.getFrom().getLabel() + " <-> " + childEdge.getTo().getLabel());
+				}
+				edgeMap.put(edgeBox.getItemCount()-1, childEdge);
+			}
+		}
+	}
+
+
+	/**
+	 * @return The original, hierarchical edge to delete all sub-Edges. One single sub-Edge to be deleted.
+	 * Null if nothing selected or aborted.
+	 */
+	public BiologicalEdgeAbstract getAnswer() {
+
+		JDialog dialog = pane.createDialog(EdgeDeleteDialog.this, "Delete a hierarchical edge");
+		//dialog.show();
+		dialog.setVisible(true);
+		Integer value = (Integer) pane.getValue();
+		if (value != null) {
+			if (value.intValue() == JOptionPane.OK_OPTION) {
+				if (selectedEdge.isSelected()) {
+					return edgeMap.get(edges.getSelectedIndex());
+				} else if (allEdges.isSelected())
+					return edgeMap.get(-1);
+			} else {
+				return null;
+			}
+		} 
+		return null;
+	}
+}
