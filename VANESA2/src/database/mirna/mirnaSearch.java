92c4c354fc72f9eb16a0555ba4d00ec06b266252
cbrinkro <cbrinkro@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2014-07-15 15:07:33 +0000

    getting miRNA search working again

--- VANESA2/src/database/mirna/mirnaSearch.java
+++ VANESA2/src/database/mirna/mirnaSearch.java
@@ -1,9 +1,14 @@
 package database.mirna;
 
+import graph.CreatePathway;
+import graph.jung.classes.MyGraph;
 import gui.MainWindow;
+import gui.MainWindowSingelton;
 import gui.ProgressBar;
 
 import java.awt.Color;
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.StringTokenizer;
@@ -15,10 +20,12 @@ import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
 import javax.swing.tree.DefaultMutableTreeNode;
 
+import biologicalElements.Pathway;
+import biologicalObjects.edges.Expression;
+import biologicalObjects.nodes.DNA;
 import biologicalObjects.nodes.Enzyme;
-
+import biologicalObjects.nodes.SRNA;
 import configurations.Wrapper;
-
 import pojos.DBColumn;
 import database.kegg.KEGGConnector;
 import database.kegg.KEGGQueries;
@@ -30,7 +37,7 @@ import database.ppi.PPIqueries;
 
 public class mirnaSearch extends SwingWorker {
 
-	private String name, acc, sequence, database;
+	private String name, acc, sequence, database, gene;
 
 	private MainWindow w;
 	private ProgressBar bar;
@@ -39,14 +46,12 @@ public class mirnaSearch extends SwingWorker {
 	private ArrayList<DBColumn> allSpecificElements = new ArrayList<DBColumn>();
 	private ArrayList<DBColumn> allKEGGPathways = new ArrayList<DBColumn>();
 
-	
-	
-	
 	public mirnaSearch(String[] input, MainWindow w, ProgressBar bar) {
 
 		name = input[0];
 		acc = input[1];
 		sequence = input[2];
+		gene = input[3];
 		database = "miRNA";
 
 		this.w = w;
@@ -59,35 +64,40 @@ public class mirnaSearch extends SwingWorker {
 		if (sequence.length() > 0 && acc.length() > 0 && name.length() > 0) {
 			String[] parameters = { "%" + sequence + "%", "%" + acc + "%",
 					"%" + name + "%" };
-			return new Wrapper().requestDbContent(6, miRNAqueries.miRNA_all,
-					parameters);
+			return new Wrapper().requestDbContent(Wrapper.dbtype_MiRNA,
+					miRNAqueries.miRNA_all, parameters);
 		} else if (sequence.length() > 0 && acc.length() > 0) {
 			String[] parameters = { "%" + sequence + "%", "%" + acc + "%" };
-			return new Wrapper().requestDbContent(6,
+			return new Wrapper().requestDbContent(Wrapper.dbtype_MiRNA,
 					miRNAqueries.miRNA_sequence_acc, parameters);
 		} else if (sequence.length() > 0 && name.length() > 0) {
 			String[] parameters = { "%" + sequence + "%", "%" + name + "%" };
-			return new Wrapper().requestDbContent(6,
+			return new Wrapper().requestDbContent(Wrapper.dbtype_MiRNA,
 					miRNAqueries.miRNA_sequence_name, parameters);
 		} else if (acc.length() > 0 && name.length() > 0) {
 			String[] parameters = { "%" + acc + "%", "%" + name + "%" };
-			return new Wrapper().requestDbContent(6,
+			return new Wrapper().requestDbContent(Wrapper.dbtype_MiRNA,
 					miRNAqueries.miRNA_acc_name, parameters);
 		} else if (sequence.length() > 0) {
 
 			String[] parameters = { "%" + sequence + "%" };
-			return new Wrapper().requestDbContent(6,
+			return new Wrapper().requestDbContent(Wrapper.dbtype_MiRNA,
 					miRNAqueries.miRNA_onlySequence, parameters);
 
 		} else if (acc.length() > 0) {
 
 			String[] parameters = { "%" + acc + "%" };
-			return new Wrapper().requestDbContent(6,
+			return new Wrapper().requestDbContent(Wrapper.dbtype_MiRNA,
 					miRNAqueries.miRNA_onlyAccession, parameters);
+
+		} else if (gene.length() > 0) {
+			String[] parameters = { "%" + gene + "%" };
+			return new Wrapper().requestDbContent(Wrapper.dbtype_MiRNA,
+					miRNAqueries.miRNA_onlyGene, parameters);
 
 		} else if (name.length() > 0) {
 			String[] parameters = { "%" + name + "%" };
-			return new Wrapper().requestDbContent(6,
+			return new Wrapper().requestDbContent(Wrapper.dbtype_MiRNA,
 					miRNAqueries.miRNA_onlyName, parameters);
 
 		}
@@ -96,14 +106,13 @@ public class mirnaSearch extends SwingWorker {
 	}
 
 	protected Object doInBackground() throws Exception {
-		w.setLockedPane(true);
+		//w.setLockedPane(true);
 		results = requestDbContent();
-		w.setLockedPane(false);
+		//w.setLockedPane(false);
 		return null;
 	}
 
 	public void done() {
-
 		Boolean continueProgress = false;
 		endSearch(w, bar);
 
@@ -117,75 +126,65 @@ public class mirnaSearch extends SwingWorker {
 		}
 
 		if (continueProgress) {
-			Vector results = mirnaResultWindow.getAnswer();
+			Vector<String[]> results = mirnaResultWindow.getAnswer();
+			//System.out.println(results.get(0)[0] + " " + results.get(0)[1]);
 			if (results.size() != 0) {
-				final Iterator it = results.iterator();
+				final Iterator<String[]> it = results.iterator();
 				while (it.hasNext()) {
-					
-					Vector keggGenesVector = new Vector<String>();
-					String[] details = (String[]) it.next();
-					String microRNA_accession = details[1];
-					String[] parsingDetails = { details[0] };
-					allSpecificElements = new Wrapper().requestDbContent(6,
-							miRNAqueries.miRNA_get_Genes, parsingDetails);
-					Boolean targets = false;
-					String targetString = "(";
-					for (DBColumn column : allSpecificElements) {
-						String[] resultDetails = (String[]) column.getColumn();
-						targetString = targetString + "'" + resultDetails[0]
-								+ "',";
-						targets = true;
-						keggGenesVector.add(resultDetails[0]);
-					}
-					targetString = targetString + "'x')";
-//System.out.println(targetString);
-					if (targets) {
+					String[] details = it.next();
+					String name = details[0];
+
+					final String QUESTION_MARK = new String("\\?");
+					String finalQueryString = miRNAqueries.miRNA_get_Genes
+							.replaceFirst(QUESTION_MARK, "'%" + name + "%'");
+					allKEGGPathways = new Wrapper().requestDbContent(
+							Wrapper.dbtype_MiRNA, finalQueryString);
+					//System.out.println(finalQueryString);
+					if (allKEGGPathways.size() > 0) {
 						
-						String[] geneDetails = { targetString };
-						final String QUESTION_MARK = new String("\\?");
-						String finalQueryString = miRNAqueries.miRNA_get_Pathways
-								.replaceFirst(QUESTION_MARK, targetString);
-						allKEGGPathways = new Wrapper().requestDbContent(2,
-								finalQueryString);
+						Pathway pw = new CreatePathway(database + " network for " + name).getPathway();
 
-						if (allKEGGPathways.size() > 0) {
-							MirnaResultKeggWindow mirnaResultKeggWindow = new MirnaResultKeggWindow(
-									allKEGGPathways);
-							Vector keggPAthwayResults = mirnaResultKeggWindow
-									.getAnswer();
-							if (keggPAthwayResults.size() != 0) {
-								String keggPathwayNumber = "";
-								String keggPathwayName = "";
-								final Iterator it3 = keggPAthwayResults
-										.iterator();
-								while (it3.hasNext()) {
+						// pw.setOrganism(organism);
+						// pw.setLink(pathwayLink);
+						// pw.setImagePath(pathwayImage);
+						// pw.setNumber(pathwayNumber);
+						MyGraph myGraph = pw.getGraph();
 
-									String[] pathwayResutls = (String[]) it3
-											.next();
-									keggPathwayNumber= "hsa"+pathwayResutls[1];
-									keggPathwayName = pathwayResutls[0];
-								}
-
-								String[] keggParamters = {keggPathwayNumber, keggPathwayName,"Homo sapiens"};
-								KEGGConnector kc=new KEGGConnector(bar, keggParamters, false);
-								kc.setMirnas(keggGenesVector);
-								kc.setMirnaName(microRNA_accession);
-
-								kc.execute();
-								//new mirnaConnector(keggGenesVector, kc.getPw());
-								
-							}
+						//stopVisualizationModel();
+						
+						SRNA root = new SRNA(name, name);
+						
+						pw.addVertex(root, new Point2D.Double(0,0));
+						
+						Iterator<DBColumn> cols = allKEGGPathways.iterator();
+						DNA dna;
+						String[] dbcol;
+						while(cols.hasNext()){
+							dbcol = cols.next().getColumn();
+							
+							dna = new DNA(dbcol[0], dbcol[0]);
+							pw.addVertex(dna, new Point2D.Double(0,0));
+							pw.addEdge(new Expression("","",root, dna));
+							
 						}
+						//startVisualizationModel();
 
-					} else {
-						JOptionPane
-								.showMessageDialog(w,
-										"Sorry, no targets recorded for this specific microRNA in TarBase.");
+						myGraph.changeToGEMLayout();
+						myGraph.fitScaleOfViewer(myGraph.getSatelliteView());
+						myGraph.normalCentering();
+						bar.closeWindow();
+
+						MainWindow window = MainWindowSingelton.getInstance();
+						window.updateOptionPanel();
+						window.setVisible(true);
+						
+						
 					}
+
 				}
 			}
 		}
-		endSearch(w, bar);
+		//endSearch(w, bar);
 	}
 
 	private void endSearch(final MainWindow w, final ProgressBar bar) {
