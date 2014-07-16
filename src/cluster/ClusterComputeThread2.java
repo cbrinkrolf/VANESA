ad8b5f7a6c75bd6033eb57699c1833d607a507a5
bniemann1 <bniemann1@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2014-07-16 13:42:24 +0000

    Update: dcb delente vertices without cliques/cycels

--- src/cluster/ClusterComputeThread2.java
+++ src/cluster/ClusterComputeThread2.java
@@ -95,139 +95,14 @@ public class ClusterComputeThread2 extends Thread {
 							JOptionPane.ERROR_MESSAGE);
 				}
 			});
-		} finally{
-			DenselyConnectedBiclusteringGUI.reactivateUI();
 		}
 
 	}
 
-	public boolean computeInBackground() {
 
-		// Catch if any input Data is given
-		if (adjmatrix == null) {
-			System.out.println("Please set adjacency data.");
-			return false;
-		}
-
-		// MARTIN: set server by job type
-		String url = "rmi://cassiopeidae/ClusterJobs";
-		// System.setProperty("java.rmi.server.hostname", "cassiopeidae");
-		// String url = "rmi://nero/Server";
-		// System.setProperty("java.rmi.server.hostname", "nero");
-		resulttable = new Hashtable<Integer, Double>();
-		try {
-			server = (IJobServer) Naming.lookup(url);
-
-			if (!server.submitJob(job, adjmatrix, helper)) {
-				JOptionPane.showMessageDialog(
-						MainWindowSingelton.getInstance(), "Queue is at maximum capacity!");
-			}
-
-		}catch (NotBoundException e) {
-			SwingUtilities.invokeLater(new Runnable() {
-				public void run() {
-					JOptionPane.showMessageDialog(MainWindowSingelton
-							.getInstance().returnFrame(),
-							"RMI Interface could not be established.", "Error",
-							JOptionPane.ERROR_MESSAGE);
-				}
-			});
-			return false;
-
-		} catch (RemoteException e) {
-			SwingUtilities.invokeLater(new Runnable() {
-				public void run() {
-					JOptionPane.showMessageDialog(MainWindowSingelton
-							.getInstance().returnFrame(),
-							"Cluster not reachable.", "Error",
-							JOptionPane.ERROR_MESSAGE);
-				}
-			});
-			return false;
-
-		} catch (MalformedURLException e) {
-			SwingUtilities.invokeLater(new Runnable() {
-				public void run() {
-					JOptionPane.showMessageDialog(MainWindowSingelton
-							.getInstance().returnFrame(),
-							"Clusteradress could not be resolved.", "Error",
-							JOptionPane.ERROR_MESSAGE);
-				}
-			});
-			return false;
-		}
-
-		return true;
-	}
 
 	public void setAdjMatrix(int adjmatrix[][]) {
 		this.adjmatrix = adjmatrix;
 	}
 
-//	public void setAdjLists(int[] nodearray, int[] edgearray) {
-//		this.nodearray = nodearray;
-//		this.edgearray = edgearray;
-//
-//	}
-//
-//	//Not used yet,
-//	//maybe done on server in future
-//	private void setupArrays() {
-//
-//		// get Graph hashmap
-//		MainWindow w = MainWindowSingelton.getInstance();
-//		GraphContainer con = ContainerSingelton.getInstance();
-//		Iterator<BiologicalNodeAbstract> it = con
-//				.getPathway(w.getCurrentPathway()).getAllNodes().iterator();
-//		HashMap<BiologicalNodeAbstract, HashMap<BiologicalNodeAbstract, BiologicalEdgeAbstract>> vertices = con
-//				.getPathway(w.getCurrentPathway()).getGraphRepresentation()
-//				.getAdjacencyList();
-//
-//		HashMap<BiologicalNodeAbstract, BiologicalEdgeAbstract> innermap;
-//		Iterator<Entry<BiologicalNodeAbstract, BiologicalEdgeAbstract>> itinner;
-//
-//		HashMap<BiologicalNodeAbstract, Integer> assignments = new HashMap<BiologicalNodeAbstract, Integer>();
-//		HashMap<Integer, BiologicalNodeAbstract> assignmentsback = new HashMap<Integer, BiologicalNodeAbstract>();
-//
-//		BiologicalNodeAbstract node;
-//		int counter = 0;
-//		while (it.hasNext()) {
-//			node = it.next();
-//			assignments.put(node, counter);
-//			assignmentsback.put(counter, node);
-//			counter++;
-//		}
-//
-//		// initialize arrays
-//		int eindex = 0, nodes = vertices.size(), edges = con
-//				.getPathway(w.getCurrentPathway()).getAllEdges().size(), tmpnodeid;
-//
-//		nodearray = new int[nodes];
-//		edgearray = new int[2 * edges]; // undirected
-//
-//		// Iterate outer map, starting on zero to n-1
-//		for (int i = 0; i < nodes; i++) {
-//			// Set current starting position of the adjacency list in edge array
-//			nodearray[i] = eindex;
-//			innermap = vertices.get(assignmentsback.get(i));
-//			itinner = innermap.entrySet().iterator();
-//			// Iterate inner map
-//			while (itinner.hasNext()) {
-//				Entry<BiologicalNodeAbstract, BiologicalEdgeAbstract> connection = itinner
-//						.next();
-//				tmpnodeid = assignments.get(connection.getKey());
-//				// System.out.println(connection.getValue().isDirected());
-//				edgearray[eindex] = tmpnodeid;
-//				eindex++;
-//			}
-//		}
-//	}
-
-	public Hashtable<Integer, Double> getResultTable() {
-		return resulttable;
-	}
-
-	public HashSet<HashSet<Integer>> getResultSet() {
-		return resultset;
-	}
 }
