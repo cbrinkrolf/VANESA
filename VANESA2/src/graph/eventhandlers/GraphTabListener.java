a564d868030d4b734a34a6b7d795a4843a322b14
toptoptop <toptoptop@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2014-07-23 11:28:16 +0000

    Tab-changing in hierarchy bug fixed.

--- VANESA2/src/graph/eventhandlers/GraphTabListener.java
+++ VANESA2/src/graph/eventhandlers/GraphTabListener.java
@@ -51,7 +51,6 @@ public class GraphTabListener implements TabListener {
 		if(window.getTabCount() > 0){
 			window.updateElementTree();
 			window.updateSatelliteView();
-			window.updateHierarchyView();
 			window.updateFilterView();
 			window.updatePathwayTree();
 			window.updateProjectProperties();
