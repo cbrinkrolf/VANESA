7e0bf377c934ef2125f339a6d754a26c21170402
toptoptop <toptoptop@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2014-07-30 17:01:05 +0000

    Enlarged environment for adding nodes to coarse nodes via drag and drop; Automatic repositioning of nodes added to coarse nodes via drag and drop;

--- VANESA2/src/graph/eventhandlers/MyPickingGraphMousePlugin.java
+++ VANESA2/src/graph/eventhandlers/MyPickingGraphMousePlugin.java
@@ -1,5 +1,7 @@
 package graph.eventhandlers;
 
+import java.awt.Rectangle;
+import java.awt.Shape;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
 import java.util.Collection;
@@ -13,72 +15,114 @@ import javax.swing.JOptionPane;
 
 import miscalleanous.internet.FollowLink;
 import biologicalElements.Elementdeclerations;
-import biologicalElements.NodeStateChanged;
 import biologicalObjects.edges.BiologicalEdgeAbstract;
 import biologicalObjects.nodes.BiologicalNodeAbstract;
 import edu.uci.ics.jung.algorithms.layout.GraphElementAccessor;
-import edu.uci.ics.jung.algorithms.layout.Layout;
-import edu.uci.ics.jung.visualization.Layer;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
-import edu.uci.ics.jung.visualization.control.AnimatedPickingGraphMousePlugin;
-//import edu.uci.ics.jung.graph.Vertex;
 import edu.uci.ics.jung.visualization.control.PickingGraphMousePlugin;
 import graph.GraphInstance;
-import graph.jung.classes.MyGraph;
 import gui.MainWindowSingelton;
 
 public class MyPickingGraphMousePlugin extends
 		PickingGraphMousePlugin<BiologicalNodeAbstract, BiologicalEdgeAbstract> {
-
+	
 	private GraphInstance graphInstance = new GraphInstance();
+	private HashMap<BiologicalNodeAbstract, Point2D> oldVertexPositions = new HashMap<BiologicalNodeAbstract, Point2D>();
 
+	
 	public void mouseReleased(MouseEvent e) {
+		
+		// If mouse was released to change the selection, save vertex positions and return.
+		if(!oldVertexPositions.keySet().containsAll(graphInstance.getPathway().getSelectedNodes()) ||
+				oldVertexPositions.keySet().size()!=graphInstance.getPathway().getSelectedNodes().size()){
+			saveOldVertexPositions(e);
+			super.mouseReleased(e);
+			return;
+		}
+		
+		Collection<BiologicalNodeAbstract> selectedNodes = graphInstance.getPathway().getSelectedNodes();
+		
+		// If no nodes were selected, return.
+		if(selectedNodes.isEmpty()){
+			super.mouseReleased(e);
+			return;
+		}
+		
+		// Find coarse nodes in specified environment of the final mouse position.
 		final VisualizationViewer<BiologicalNodeAbstract, BiologicalEdgeAbstract> vv = (VisualizationViewer<BiologicalNodeAbstract, BiologicalEdgeAbstract>) e
 				.getSource();
-		final Point2D p = vv.getRenderContext().getMultiLayerTransformer()
-				.inverseTransform(e.getPoint());
 		GraphElementAccessor<BiologicalNodeAbstract, BiologicalEdgeAbstract> pickSupport = vv
 				.getPickSupport();
+		float width = selectedNodes.iterator().next().getShape().getBounds().width;
+		float height = selectedNodes.iterator().next().getShape().getBounds().height;
+		Shape shape = new Rectangle.Float(e.getX()-(width/2), e.getY()-(height/2), width, height);
+		Collection<BiologicalNodeAbstract> vertices =  pickSupport.getVertices(vv.getGraphLayout(), shape);
+		vertices.removeAll(selectedNodes);
+		
+		// If exactly one node was found, take this node, otherwise return.
 		BiologicalNodeAbstract vertex = null;
-		Collection<BiologicalNodeAbstract> selectedNodes = GraphInstance.getMyGraph().getVisualizationViewer().getPickedVertexState().getPicked();
-		vertex = (BiologicalNodeAbstract) pickSupport
-				.getVertex(vv.getGraphLayout(), e.getPoint().getX(), e
-						.getPoint().getY());
-		if(vertex!=null && vertex.isCoarseNode() && !selectedNodes.contains(vertex)){
+		if(vertices.size()==1){
+			vertex = vertices.iterator().next();
+		} else {
+			super.mouseReleased(e);
+			return;
+		}
+		
+		// If node is a coarse node and not contained in the selection, add the selection to the coarse node.
+		if(vertex.isCoarseNode() && !selectedNodes.contains(vertex)){
+			// Disallow to add selection to environment coarse nodes.
 			if(graphInstance.getPathway().isBNA()){
-				if(((BiologicalNodeAbstract)graphInstance.getPathway()).getEnvironment().contains(vertex)){
+				if(((BiologicalNodeAbstract)graphInstance.getPathway()).getEnvironment().contains(vertices)){
 					JOptionPane.showMessageDialog(MainWindowSingelton.getInstance(), 
 							"Not possible to add nodes to environment nodes.", 
 							"Coarse node integration Error!", JOptionPane.ERROR_MESSAGE);
+					oldVertexPositions.clear();
 					super.mouseReleased(e);
 					return;
 				}
 			}
 			coarseNodeFusion(vertex);
 		}
+		oldVertexPositions.clear();
 		super.mouseReleased(e);
-
 	}
 
-	public void coarseNodeFusion(BiologicalNodeAbstract vertex){
+	private void coarseNodeFusion(BiologicalNodeAbstract vertex){
 		Set<BiologicalNodeAbstract> selection = new HashSet<BiologicalNodeAbstract>();
-		selection.addAll(GraphInstance.getMyGraph().getVisualizationViewer().getPickedVertexState().getPicked());
+		selection.addAll(graphInstance.getPathway().getSelectedNodes());
 		Set<BiologicalNodeAbstract> coarseNodes = new HashSet<BiologicalNodeAbstract>();
 		coarseNodes.addAll(selection);
 		coarseNodes.addAll(vertex.getInnerNodes());
 		vertex.flat();
 		if(vertex.computeCoarseType(coarseNodes)!=null){
 			vertex.coarse(coarseNodes);
+			for(BiologicalNodeAbstract node : selection){
+				System.out.println(oldVertexPositions.size());
+				vertex.getGraph().moveVertex(node, oldVertexPositions.get(node).getX(), oldVertexPositions.get(node).getY());
+			}
 		}
 	}
-	// Iterator it = graphInstance.getMyGraph().getAllvertices().iterator();
-	// Iterator it = graphInstance.getMyGraph().getAllEdges().iterator();
-	// while(it.hasNext()){
-	// System.out.println(graphInstance.getMyGraph().getVisualizationViewer().getPickedEdgeState().isPicked(it.next()));
-	// }
-	// System.out.println("rel E: "+graphInstance.getMyGraph().getVisualizationViewer().getPickedEdgeState().getSelectedObjects().length);
-	// System.out.println("rel V: "+graphInstance.getMyGraph().getVisualizationViewer().getPickedVertexState().getSelectedObjects().length);
-
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+		//oldVertexPosition = graphInstance.getPathway().getGraph().getVertexLocation(pickSupport.getVertex(vv.getGraphLayout(), e.getPoint().getX(), e.getPoint().getY()));
+		super.mousePressed(e);
+		saveOldVertexPositions(e);
+	};
+	
+	private void saveOldVertexPositions(MouseEvent e){
+		oldVertexPositions.clear();
+		final VisualizationViewer<BiologicalNodeAbstract, BiologicalEdgeAbstract> vv = (VisualizationViewer<BiologicalNodeAbstract, BiologicalEdgeAbstract>) e
+				.getSource();
+		GraphElementAccessor<BiologicalNodeAbstract, BiologicalEdgeAbstract> pickSupport = vv
+				.getPickSupport();
+		for(BiologicalNodeAbstract vertex : graphInstance.getPathway().getSelectedNodes()){
+			oldVertexPositions.put(vertex, graphInstance.getPathway().getGraph().getVertexLocation(vertex));
+		}
+	}
+	
+	
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
 
