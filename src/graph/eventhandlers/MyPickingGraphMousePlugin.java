666fc1b888f69ca3caf8ff89555a429ea708a02c
toptoptop <toptoptop@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2014-08-04 16:09:48 +0000

    (very) huge code improvements, clean-up and commentation in bna. Real adding nodes to coarse nodes function (still problems when adding coarse nodes to coarse nodes).

--- src/graph/eventhandlers/MyPickingGraphMousePlugin.java
+++ src/graph/eventhandlers/MyPickingGraphMousePlugin.java
@@ -90,16 +90,27 @@ public class MyPickingGraphMousePlugin extends
 	private void coarseNodeFusion(BiologicalNodeAbstract vertex){
 		Set<BiologicalNodeAbstract> selection = new HashSet<BiologicalNodeAbstract>();
 		selection.addAll(graphInstance.getPathway().getSelectedNodes());
-		Set<BiologicalNodeAbstract> coarseNodes = new HashSet<BiologicalNodeAbstract>();
-		coarseNodes.addAll(selection);
-		coarseNodes.addAll(vertex.getInnerNodes());
-		vertex.flat();
-		if(vertex.computeCoarseType(coarseNodes)!=null){
-			vertex.coarse(coarseNodes);
+		if(vertex.tryAddToCoarseNode(selection)){
 			for(BiologicalNodeAbstract node : selection){
-				vertex.getGraph().moveVertex(node, oldVertexPositions.get(node).getX(), oldVertexPositions.get(node).getY());
+				vertex.getGraph().moveVertex(node, oldVertexPositions.get(node).getX(), 
+						oldVertexPositions.get(node).getY());
+			}
+		} else {
+			for(BiologicalNodeAbstract node : selection){
+				graphInstance.getPathway().getGraph().moveVertex(node, oldVertexPositions.get(node).getX(), 
+						oldVertexPositions.get(node).getY());
 			}
 		}
+//		Set<BiologicalNodeAbstract> coarseNodes = new HashSet<BiologicalNodeAbstract>();
+//		coarseNodes.addAll(selection);
+//		coarseNodes.addAll(vertex.getInnerNodes());
+//		vertex.flat();
+//		if(vertex.computeCoarseType(coarseNodes)!=null){
+//			vertex.tryAddToCoarseNode(coarseNodes);
+//			for(BiologicalNodeAbstract node : selection){
+//				vertex.getGraph().moveVertex(node, oldVertexPositions.get(node).getX(), oldVertexPositions.get(node).getY());
+//			}
+//		}
 	}
 	
 	@Override
