7540b678852025efaa343827f7dd1d98be10f5be
cbrinkro <cbrinkro@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2014-08-25 17:59:58 +0000

    PNML Export, thanks to Kerstin :)

--- VANESA2/src/io/SaveDialog.java
+++ VANESA2/src/io/SaveDialog.java
@@ -1,18 +1,36 @@
 package io;
 
+import fr.lip6.move.pnml.framework.utils.exception.InvalidIDException;
+import fr.lip6.move.pnml.framework.utils.exception.VoidRepositoryException;
 import gonOutput.GONoutput;
+import graph.ContainerSingelton;
+import graph.GraphContainer;
 import graph.GraphInstance;
+import gui.MainWindow;
 import gui.MainWindowSingelton;
 import io.graphML.SaveGraphML;
 
+import java.awt.HeadlessException;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
+import org.slf4j.LoggerFactory;
+
+import petriNet.ConvertToPetriNet;
+import petriNet.PNEdge;
+import petriNet.Place;
+import petriNet.Transition;
+import biologicalObjects.edges.BiologicalEdgeAbstract;
+import biologicalObjects.nodes.BiologicalNodeAbstract;
+import ch.qos.logback.classic.LoggerContext;
 import moOutput.MOoutput;
 import xmlOutput.sbml.JSBMLoutput;
+import xmlOutput.sbml.PNMLOutput;
 import xmlOutput.sbml.SBMLoutput;
 import xmlOutput.sbml.SBMLoutputNoWS;
 import xmlOutput.sbml.VAMLoutput;
@@ -27,12 +45,16 @@ public class SaveDialog {
 	private boolean vaBool = true;
 	private boolean txtBool = true;
 	private boolean itxtBool = true;
+	private boolean pnmlBool = true;
 
 	private String fileFormat;
 	private File file;
 
 	private String sbmlDescription = "System Biology Markup Language (*.sbml)";
 	private String sbml = "sbml";
+	
+	private String pnmlDescription = "Petri Net Markup Language (*.pnml)";
+	private String pnml = "pnml";
 
 	private String vamlDescription = "VANESA Markup Language (*.vaml)";
 	private String vaml = "vaml";
@@ -65,6 +87,7 @@ public class SaveDialog {
 	public static int FORMAT_VA = 16;
 	public static int FORMAT_TXT = 32;
 	public static int FORMAT_ITXT = 64;
+	public static int FORMAT_PNML = 128;
 
 	public SaveDialog(int format) {
 
@@ -75,6 +98,7 @@ public class SaveDialog {
 		vaBool = (format & FORMAT_VA) == FORMAT_VA;
 		txtBool = (format & FORMAT_TXT) == FORMAT_TXT;
 		itxtBool = (format & FORMAT_ITXT) == FORMAT_ITXT;
+		pnmlBool = (format & FORMAT_PNML) == FORMAT_PNML;
 
 		if (ConnectionSettings.getFileDirectory() != null) {
 			chooser = new JFileChooser(ConnectionSettings.getFileDirectory());
@@ -103,7 +127,9 @@ public class SaveDialog {
 			chooser
 					.addChoosableFileFilter(new MyFileFilter(csml,
 							csmlDescription));
-
+		if (pnmlBool)
+			chooser.addChoosableFileFilter(new MyFileFilter(pnml,
+					pnmlDescription));
 		if (vaBool)
 			chooser.addChoosableFileFilter(new MyFileFilter(vaml,
 					vamlDescription));
@@ -189,7 +215,80 @@ public class SaveDialog {
 			JOptionPane.showMessageDialog(MainWindowSingelton.getInstance(),
 					txtDescription + " File saved");
 
-		} else if (fileFormat.equals(csmlDescription)) {
+		} else if (fileFormat.equals(pnmlDescription)) {
+			GraphContainer con = ContainerSingelton.getInstance();
+			MainWindow w = MainWindowSingelton.getInstance();
+			if (!con.isPetriView()){
+				/*ConvertToPetriNet convertToPetriNet = new ConvertToPetriNet();
+				getCorrectFile(pnml);
+				PNMLOutput pnmlOutput = new PNMLOutput(file, convertToPetriNet.getBiologicalEdges(),convertToPetriNet.getBiologicalNodes(),convertToPetriNet.getBiologicalTransitions());
+				
+				try {
+					JOptionPane.showMessageDialog(
+							MainWindowSingelton.getInstance(),pnmlDescription
+									+ pnmlOutput.generatePNMLDocument() + " File saved");
+				} catch (HeadlessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InvalidIDException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (VoidRepositoryException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}*/
+			} else {
+				
+				getCorrectFile(pnml);
+				BiologicalEdgeAbstract bea;
+				ArrayList <PNEdge> edgeList = new ArrayList <PNEdge>();
+				Iterator<BiologicalEdgeAbstract> itEdge = con.getPathway(w.getCurrentPathway()).getAllEdges().iterator();
+				while (itEdge.hasNext()) {
+					bea = itEdge.next();
+					if(bea instanceof PNEdge){
+					edgeList.add((PNEdge)bea);
+					}
+
+				}
+				BiologicalNodeAbstract bna;
+				ArrayList <Place> nodeList = new ArrayList <Place>();
+				ArrayList <Transition> transitionList = new ArrayList <Transition>();
+				Iterator<BiologicalNodeAbstract> itNode = con.getPathway(w.getCurrentPathway()).getAllNodes().iterator();
+			
+				while (itNode.hasNext()) {
+					bna = itNode.next();
+					if(bna instanceof Place){
+						nodeList.add((Place)bna);
+					} else if(bna instanceof Transition) {
+						 transitionList.add((Transition)bna);
+					}
+				}
+
+				PNMLOutput pnmlOutput = new PNMLOutput(file, edgeList, nodeList,transitionList);
+				
+				try {
+					JOptionPane.showMessageDialog(
+							MainWindowSingelton.getInstance(),pnmlDescription
+									+ pnmlOutput.generatePNMLDocument() + "File saved. File validated and saved.");
+					
+					if(pnmlOutput.getFinished()){
+						LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+						loggerContext.stop();
+					}
+					
+				} catch (HeadlessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InvalidIDException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (VoidRepositoryException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			} 
+		}else if (fileFormat.equals(csmlDescription)) {
 			getCorrectFile(csml);
 			new GONoutput(file, new GraphInstance().getPathway());
 			JOptionPane.showMessageDialog(MainWindowSingelton.getInstance(),
