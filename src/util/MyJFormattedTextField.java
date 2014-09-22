150cf2ac5bc1057d4739eafaa68b750dd96c0119
cbrinkro <cbrinkrolf@users.noreply.github.com>
2014-09-22 07:54:11 +0000

    nullpointer fix

--- src/util/MyJFormattedTextField.java
+++ src/util/MyJFormattedTextField.java
@@ -5,7 +5,6 @@ import java.text.ParseException;
 
 import javax.swing.JFormattedTextField;
 
-
 public class MyJFormattedTextField extends JFormattedTextField {
 
 	/**
@@ -13,34 +12,36 @@ public class MyJFormattedTextField extends JFormattedTextField {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public MyJFormattedTextField(){
+	public MyJFormattedTextField() {
 		super();
 	}
-	
-	public MyJFormattedTextField(Format format){
+
+	public MyJFormattedTextField(Format format) {
 		super(format);
 	}
-	
-	public MyJFormattedTextField(AbstractFormatter formatter){
+
+	public MyJFormattedTextField(AbstractFormatter formatter) {
 		super(formatter);
 	}
-	
-	public MyJFormattedTextField(AbstractFormatterFactory aff){
+
+	public MyJFormattedTextField(AbstractFormatterFactory aff) {
 		super(aff);
 	}
-	
-	
-	
+
 	@Override
 	public Object getValue() {
 		try {
 			this.commitEdit();
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
-			//e.printStackTrace();
+			// e.printStackTrace();
 		}
-		//this.setValue(((Number) super.getValue()).doubleValue());
-		return Math.abs(((Number)super.getValue()).doubleValue());
+		// this.setValue(((Number) super.getValue()).doubleValue());
+		if ((super.getValue()) != null) {
+			return Math.abs(((Number) super.getValue()).doubleValue());
+		} else {
+			return super.getValue();
+		}
 	}
 
 }
