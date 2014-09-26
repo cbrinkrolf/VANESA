91960ea7ebe07bc19ce044e9a04d5ef0d421035b
mlewinsk <mlewinsk@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2014-09-26 06:37:33 +0000

    Computed graph theoretic values are now stored in the BiologicalNodeAbstract and can be retrieved for future purposes.

---
+++ src/graph/algorithms/NodeAttributeTypes.java
@@ -0,0 +1,7 @@
+package graph.algorithms;
+
+public class NodeAttributeTypes{
+	public static final int EXPERIMENT =1;
+	public static final int GRAPH_PROPERTY =2;
+	public static final int COLOR =3;
+}
