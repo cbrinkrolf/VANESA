54a01869d4f5395da2cc38f994b47a9d1bd42f7f
toptoptop <toptoptop@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2014-11-12 14:49:25 +0000

    Bugfix in computing angles between two points. Much easier way of computing control Points for edges in HEBLayout.

--- src/graph/layouts/hebLayout/Circle.java
+++ src/graph/layouts/hebLayout/Circle.java
@@ -1,6 +1,9 @@
 package graph.layouts.hebLayout;
 
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+
+import edu.uci.ics.jung.graph.util.Pair;
 
 public class Circle {
 	
@@ -24,7 +27,45 @@ public class Circle {
 	 * @author tloka
 	 */
 	public static double getAngle(Point2D centerPoint, Point2D nodePosition){
-		double angle = Math.atan2(nodePosition.getY()-centerPoint.getY(), nodePosition.getX()-centerPoint.getY());
-		return convertAngle(angle);
+		double angle = Math.atan2(nodePosition.getY()-centerPoint.getY(), nodePosition.getX()-centerPoint.getX());
+		return angle;//convertAngle(angle);
+	}
+	
+	public static Point2D getPointOnCircle(Point2D centerPoint, double radius, double angle){
+		return new Point2D.Double(Math.cos(angle) * radius + centerPoint.getX(),
+				Math.sin(angle) * radius + centerPoint.getY());
+	}
+	
+//	public static double getAngleDifference(double startAngle, double endAngle){
+//		double start = convertAngle(startAngle);
+//		double end = convertAngle(endAngle);
+//		if(start==end)
+//			return 0;
+//		if(end>start){
+//			if(end>start+Math.PI){
+//				return -(start+2*Math.PI-end);
+//			} else {
+//				return end-start;
+//			}
+//		} else {
+//			if(start>end+Math.PI){
+//				return end+2*Math.PI-start;
+//			} else {
+//				return -(start-end);
+//			}
+//		}
+//	}
+	
+	/**
+	 * Gives the Point coordinates in a rotated coordinate system.
+	 * @param point
+	 * @param angle
+	 * @return Point coordinates in rotated coordinate system.
+	 */
+	public static Point2D rotatePoint(Point2D point, double angle){
+		Point2D rotatedPoint = new Point2D.Double(0,0);
+		rotatedPoint.setLocation(-Math.sin(angle)*point.getX()+Math.cos(angle)*point.getY(), 
+				Math.cos(angle)*point.getX()+Math.sin(angle*point.getY()));
+		return rotatedPoint;
 	}
 }
