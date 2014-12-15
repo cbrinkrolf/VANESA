cf4e19d1c9cd6f1fdecc47eab9a00d64025bfd57
toptoptop <toptoptop@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2014-12-15 12:33:36 +0000

    Multiple node selection options in HCTLayout.

--- src/graph/layouts/hctLayout/HCTLayoutConfig.java
+++ src/graph/layouts/hctLayout/HCTLayoutConfig.java
@@ -1,10 +1,14 @@
 package graph.layouts.hctLayout;
 
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
@@ -12,7 +16,7 @@ import javax.swing.event.ChangeListener;
 
 import graph.layouts.HierarchicalCircleLayoutConfig;
 
-public class HCTLayoutConfig extends HierarchicalCircleLayoutConfig implements ChangeListener{
+public class HCTLayoutConfig extends HierarchicalCircleLayoutConfig implements ChangeListener,ActionListener{
 	
 	/**
 	 * 
@@ -26,6 +30,8 @@ public class HCTLayoutConfig extends HierarchicalCircleLayoutConfig implements C
 	public static JCheckBox autorelayout;
 	public static JCheckBox moveInGroups;
 	public static JSlider groupSeperationSlider;
+	public static JComboBox<String> groupSelection;
+	public static GroupSelection SELECTION;
 	
 	public HCTLayoutConfig() {
 		super(HCTLayout.class);
@@ -69,6 +75,21 @@ public class HCTLayoutConfig extends HierarchicalCircleLayoutConfig implements C
 		moveInGroups.setToolTipText("Selects the whole group if a node is selected. So, nodes can only be moved groupwise.");
 		moveInGroups.setSelected(true);
 		groupPreferences.add(moveInGroups);
+		
+		 JPanel nodeSelection = new JPanel();
+		 BoxLayout nodeSelectionLayout = new BoxLayout(nodeSelection, BoxLayout.PAGE_AXIS);
+		 nodeSelection.setLayout(nodeSelectionLayout);
+		 nodeSelection.setBorder(BorderFactory.createTitledBorder("Node Selection"));
+		 
+		 String[] groupSelectionOptions = {"Single Node","Node Path", "Node Subpath", "Node Group"};
+		 groupSelection = new JComboBox<String>(groupSelectionOptions);
+		 groupSelection.setPreferredSize(new Dimension(200,30));
+		 groupSelection.setMaximumSize(groupSelection.getPreferredSize());
+		 groupSelection.setSelectedIndex(3);
+		 groupSelection.addActionListener(this);
+		 nodeSelection.add(groupSelection);
+		 
+		 groupPreferences.add(nodeSelection);
 	
 		 add(groupPreferences);
 		
@@ -110,4 +131,27 @@ public class HCTLayoutConfig extends HierarchicalCircleLayoutConfig implements C
 			HCTLayoutConfig.GROUP_DISTANCE_FACTOR = HCTLayoutConfig.groupSeperationSlider.getValue();
 		}
 	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		if (arg0.getSource().equals(HCTLayoutConfig.groupSelection)) {
+			if(HCTLayoutConfig.groupSelection.getSelectedItem().equals("Single Node")){
+				HCTLayoutConfig.SELECTION = GroupSelection.SINGLE;
+			} else if(HCTLayoutConfig.groupSelection.getSelectedItem().equals("Node Path")){
+				HCTLayoutConfig.SELECTION = GroupSelection.PATH;
+			} else if(HCTLayoutConfig.groupSelection.getSelectedItem().equals("Node Subpath")){
+				HCTLayoutConfig.SELECTION = GroupSelection.SUBPATH;
+			} else if(HCTLayoutConfig.groupSelection.getSelectedItem().equals("Node Group")){
+				HCTLayoutConfig.SELECTION = GroupSelection.GROUP;
+			}
+		}	
+	}
+	
+	public enum GroupSelection{
+		SINGLE,
+		PATH,
+		SUBPATH,
+		GROUP
+	}
 }
+
