8af131c0788ff07227c78ff3eb2b8a05d95bba40
toptoptop <toptoptop@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2015-02-11 10:56:36 +0000

    BRENDA autocoarse without cofactors and inhibitors.

--- src/database/brenda/BrendaTree.java
+++ src/database/brenda/BrendaTree.java
@@ -1,12 +1,21 @@
 package database.brenda;
 
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
+
+import biologicalObjects.nodes.BiologicalNodeAbstract;
 
 public class BrendaTree {
 
 	private DefaultTreeModel model;
 	private DefaultMutableTreeNode root;
+	private HashMap<BiologicalNodeAbstract, DefaultMutableTreeNode> enzyme2node = new HashMap<BiologicalNodeAbstract, DefaultMutableTreeNode>();
+	private HashMap<DefaultMutableTreeNode, BiologicalNodeAbstract> node2enzyme = new HashMap<DefaultMutableTreeNode, BiologicalNodeAbstract>();
 	
 	public BrendaTree(){
 		root = new DefaultMutableTreeNode("BRENDA Pathway");
@@ -17,12 +26,43 @@ public class BrendaTree {
 		return root;
 	}
 	
-	public void addNode(DefaultMutableTreeNode parentNode,DefaultMutableTreeNode newChildNode){
+	public void addNode(DefaultMutableTreeNode parentNode,DefaultMutableTreeNode newChildNode, BiologicalNodeAbstract bna){
 		
 		parentNode.add(newChildNode);
 		model.nodeStructureChanged(root); 
 		model.reload();
-
+		if(!enzyme2node.containsKey(bna))
+			enzyme2node.put(bna, newChildNode);
+		if(!node2enzyme.containsKey(newChildNode))
+			node2enzyme.put(newChildNode, bna);
+	}
+	
+	public BiologicalNodeAbstract getEnzyme(DefaultMutableTreeNode node){
+		return node2enzyme.get(node);
+	}
+	
+	public DefaultMutableTreeNode getTreeNode(BiologicalNodeAbstract bna){
+		return enzyme2node.get(bna);
+	}
+	
+	public void printTree(){
+		Enumeration<DefaultMutableTreeNode> children = getRoot().children();
+		while(children.hasMoreElements()){
+			printSubTree((DefaultMutableTreeNode) children.nextElement());
+		}
+	}
+	
+	private void printSubTree(DefaultMutableTreeNode node){
+		if(node.getParent() != getRoot())
+			System.out.println(node2enzyme.get(node.getParent()).getLabel() + "->" + node2enzyme.get(node).getLabel());
+		else
+			System.out.println("root" + "->" + node2enzyme.get(node).getLabel());
+		Enumeration<DefaultMutableTreeNode> children = node.children();
+		while(children.hasMoreElements()){
+			printSubTree((DefaultMutableTreeNode) children.nextElement());
+		}
+		if(!node.isLeaf())
+			System.out.println();
 	}
 	
 }
