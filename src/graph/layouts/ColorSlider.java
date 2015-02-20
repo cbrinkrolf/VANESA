08c0ce0889d2d7e8b59f055ce6588d4182575a1b
toptoptop <toptoptop@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2015-02-20 16:42:07 +0000

    Color Slider update for new LaF.

--- src/graph/layouts/ColorSlider.java
+++ src/graph/layouts/ColorSlider.java
@@ -1,9 +1,24 @@
 package graph.layouts;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Stroke;
+import java.awt.geom.GeneralPath;
 
 import javax.swing.BorderFactory;
+import javax.swing.JComponent;
 import javax.swing.JSlider;
+import javax.swing.SwingConstants;
+import javax.swing.plaf.basic.BasicSliderUI;
+
+import com.jtattoo.plaf.AbstractLookAndFeel;
+import com.jtattoo.plaf.ColorHelper;
+import com.jtattoo.plaf.JTattooUtilities;
+import com.jtattoo.plaf.aluminium.AluminiumSliderUI;
 
 public class ColorSlider extends JSlider{
 
@@ -14,12 +29,11 @@ public class ColorSlider extends JSlider{
 		setMinimum(0);
 		setMaximum(240);
 		setValue(value);
-		setBackground(getColor());
+		setUI(new ColorSliderUI(this));
 	}
 	
 	@Override
 	public void setValue(int val){
-		setBackground(getColor());
 		super.setValue(val);
 	}
 	
@@ -80,4 +94,126 @@ public class ColorSlider extends JSlider{
 		Color col = getColor();
 		return new Color(col.getRed(),col.getGreen(),col.getBlue(),opacity);
 	}
+	
+	class ColorSliderUI extends AluminiumSliderUI
+	{
+	    
+	    ColorSliderUI(JSlider s) {
+	        super(s);
+	    }
+	 
+	    @Override
+	    public void paint( Graphics g, JComponent c ) {
+	        recalculateIfInsetsChanged();
+	        recalculateIfOrientationChanged();
+	        Rectangle clip = g.getClipBounds();
+	 
+	        if ( slider.getPaintTrack() && clip.intersects( trackRect ) ) {
+	            paintTrack( g );
+	        }
+	        if ( slider.getPaintTicks() && clip.intersects( tickRect ) ) {
+	            paintTicks( g );
+	        }
+	        if ( slider.getPaintLabels() && clip.intersects( labelRect ) ) {
+	            paintLabels( g );
+	        }
+	        if ( slider.hasFocus() && clip.intersects( focusRect ) ) {
+	            paintFocus( g );     
+	        }
+	        paintThumb( g );
+	    }
+
+	    @Override
+	    public void paintTrack(Graphics g) {
+	    	boolean leftToRight = JTattooUtilities.isLeftToRight(slider);
+	    	g.translate(trackRect.x, trackRect.y);
+	    	int overhang = 4;
+	    	int trackLeft = 0;
+	    	int trackTop = 0;
+	    	int trackRight = 0;
+	    	int trackBottom = 0;
+	    	if (slider.getOrientation() == JSlider.HORIZONTAL) {
+	    	trackBottom = (trackRect.height - 1) - overhang;
+	    	trackTop = trackBottom - (getTrackWidth() - 1);
+	    	trackRight = trackRect.width - 1;
+	    	} else {
+	    	if (leftToRight) {
+	    	trackLeft = (trackRect.width - overhang) - getTrackWidth();
+	    	trackRight = (trackRect.width - overhang) - 1;
+	    	} else {
+	    	trackLeft = overhang;
+	    	trackRight = overhang + getTrackWidth() - 1;
+	    	}
+	    	trackBottom = trackRect.height - 1;
+	    	}
+	    	g.setColor(getColor());
+	    	g.drawRect(trackLeft, trackTop, (trackRight - trackLeft) - 1, (trackBottom - trackTop) - 1);
+	    	int middleOfThumb = 0;
+	    	int fillTop = 0;
+	    	int fillLeft = 0;
+	    	int fillBottom = 0;
+	    	int fillRight = 0;
+	    	if (slider.getOrientation() == JSlider.HORIZONTAL) {
+	    	middleOfThumb = thumbRect.x + (thumbRect.width / 2);
+	    	middleOfThumb -= trackRect.x;
+	    	fillTop = trackTop + 1;
+	    	fillBottom = trackBottom - 2;
+	    	if (!drawInverted()) {
+	    	fillLeft = trackLeft + 1;
+	    	fillRight = middleOfThumb;
+	    	} else {
+	    	fillLeft = middleOfThumb;
+	    	fillRight = trackRight - 2;
+	    	}
+	    	Color colors[] = null;
+	    	if (!JTattooUtilities.isActive(slider)) {
+	    	colors = AbstractLookAndFeel.getTheme().getInActiveColors();
+	    	} else {
+	    	if (slider.isEnabled()) {
+	    	colors = AbstractLookAndFeel.getTheme().getSliderColors();
+	    	} else {
+	    	colors = AbstractLookAndFeel.getTheme().getDisabledColors();
+	    	}
+	    	}
+	    	colors = new Color[1];
+	    	colors[0] = getColor();
+	    	JTattooUtilities.fillHorGradient(g, colors, fillLeft + 2, fillTop + 2, fillRight - fillLeft - 2, fillBottom - fillTop - 2);
+	    	Color cHi = ColorHelper.darker(getColor(), 5);
+	    	Color cLo = ColorHelper.darker(getColor(), 10);
+	    	JTattooUtilities.draw3DBorder(g, cHi, cLo, fillLeft + 1, fillTop + 1, fillRight - fillLeft - 1, fillBottom - fillTop - 1);
+	    	} else {
+	    	middleOfThumb = thumbRect.y + (thumbRect.height / 2);
+	    	middleOfThumb -= trackRect.y;
+	    	fillLeft = trackLeft + 1;
+	    	fillRight = trackRight - 2;
+	    	if (!drawInverted()) {
+	    	fillTop = middleOfThumb;
+	    	fillBottom = trackBottom - 2;
+	    	} else {
+	    	fillTop = trackTop + 1;
+	    	fillBottom = middleOfThumb;
+	    	}
+	    	Color colors[] = null;
+	    	if (!JTattooUtilities.isActive(slider)) {
+	    	colors = AbstractLookAndFeel.getTheme().getInActiveColors();
+	    	} else {
+	    	if (slider.isEnabled()) {
+	    	colors = AbstractLookAndFeel.getTheme().getSliderColors();
+	    	} else {
+	    	colors = AbstractLookAndFeel.getTheme().getDisabledColors();
+	    	}
+	    	}
+	    	colors = new Color[1];
+	    	colors[0] = getColor();
+	    	JTattooUtilities.fillVerGradient(g, colors, fillLeft + 2, fillTop + 2, fillRight - fillLeft - 2, fillBottom - fillTop - 2);
+	    	Color cHi = ColorHelper.darker(getColor(), 5);
+	    	Color cLo = ColorHelper.darker(getColor(), 10);
+	    	JTattooUtilities.draw3DBorder(g, cHi, cLo, fillLeft + 1, fillTop + 1, fillRight - fillLeft - 1, fillBottom - fillTop - 1);
+	    	}
+	    	g.translate(-trackRect.x, -trackRect.y);
+	        setForeground(getColor());
+	    }
+
+	}
+	
 }
