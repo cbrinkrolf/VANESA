b5c10e5073c19a227d14ef33834d2b8cfd123e8b
mlewinsk <mlewinsk@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2015-03-16 11:27:29 +0000

    Updated cluster RMI connection and designed interfaces.

---
+++ src/cluster/clientimpl/ComputeCallback.java
@@ -0,0 +1,59 @@
+package cluster.clientimpl;
+
+import graph.algorithms.gui.GraphColoringGUI;
+
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+
+import cluster.slave.IComputeCallback;
+import cluster.slave.LayoutPoint2D;
+
+public class ComputeCallback extends UnicastRemoteObject implements Serializable, IComputeCallback {
+
+	private static final long serialVersionUID = -5452379957017610971L;
+
+	private GraphColoringGUI gui;
+
+	public ComputeCallback(GraphColoringGUI gui) throws RemoteException {
+		super();
+		this.gui = gui;
+	}
+
+	@Override
+	public void progressNotify(String message) throws RemoteException {
+		if (GraphColoringGUI.progressbar != null) {
+			GraphColoringGUI.progressbar.setProgressBarString(message);
+		}
+	}
+
+	@Override
+	public void setResultTable(Hashtable<Integer, Double> table, int jobtype)
+			throws RemoteException {
+		gui.returnComputeData(table, jobtype);
+		gui.reactiveateUI();
+	}
+
+	@Override
+	public void setResultSet(HashSet<HashSet<Integer>> set, int jobtype)
+			throws RemoteException {
+		gui.returnComputeData(set, jobtype);
+		gui.reactiveateUI();
+	}
+
+	@Override
+	public void setResultMatrix(int[][] matrix) throws RemoteException {
+
+	}
+
+	@Override
+	public void setResultCoordinates(HashMap<Integer, LayoutPoint2D> coords)
+			throws RemoteException {
+		gui.realignNetwork(coords);
+		gui.reactiveateUI();
+		
+	}
+}
