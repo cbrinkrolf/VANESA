bbebaab63fcff2f6106be7a14b094e5c9887c06b
toptoptop <toptoptop@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2015-03-28 12:36:27 +0000

    Improved autoCoarse by splitting node algorithm.

--- src/graph/hierarchies/AutoCoarse.java
+++ src/graph/hierarchies/AutoCoarse.java
@@ -1,8 +1,6 @@
 package graph.hierarchies;
 
-import gui.MainWindowSingleton;
-
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -13,137 +11,198 @@ import biologicalObjects.nodes.BiologicalNodeAbstract;
 
 public class AutoCoarse {
 	
-	public static void coarseSeperatedSubgraphs(Pathway pw){
-		
-		// Take only inner nodes.
-		Set<BiologicalNodeAbstract> nodes = new HashSet<BiologicalNodeAbstract>();
-		
-		if(pw.isBNA()){
-			nodes.addAll(((BiologicalNodeAbstract) pw).getInnerNodes());
-		} else {
-			nodes.addAll(pw.getAllNodes());
-		}
-		
-		// Initialisation of neighbors and Subgraph lists.
-		HashMap<Integer, List<List<BiologicalNodeAbstract>>> subgraphs = new HashMap<Integer, List<List<BiologicalNodeAbstract>>>();
-		List<BiologicalNodeAbstract> subgraph = new ArrayList<BiologicalNodeAbstract>();
-		List<List<BiologicalNodeAbstract>> theSubgraphs = new ArrayList<List<BiologicalNodeAbstract>>();
-		HashMap<Integer, List<BiologicalNodeAbstract>> neighbors = new HashMap<Integer, List<BiologicalNodeAbstract>>();
-		List<BiologicalNodeAbstract> neigh = new ArrayList<BiologicalNodeAbstract>();
-		
-		for(BiologicalNodeAbstract n : nodes){
-			neigh = new ArrayList<BiologicalNodeAbstract>();
-			neigh.addAll(pw.getGraph().getJungGraph().getNeighbors(n));
-			// delete environment nodes.
-			neigh.retainAll(nodes);
-			neighbors.put(n.getID(), neigh);
-			theSubgraphs = new ArrayList<List<BiologicalNodeAbstract>>();
-			for(BiologicalNodeAbstract neighbor : neigh){
-				subgraph = new ArrayList<BiologicalNodeAbstract>();
-				subgraph.add(neighbor);
-				theSubgraphs.add(subgraph);
-			}
-			subgraphs.put(n.getID(),theSubgraphs);
-		}
-		
-		// Compute seperating nodes with more than 2 neighbors
-		HashSet<BiologicalNodeAbstract> sortedNodes = new HashSet<BiologicalNodeAbstract>();
-		HashSet<BiologicalNodeAbstract> retainCheck = new HashSet<BiologicalNodeAbstract>();
-		boolean somethingChanged= true;
-		for(BiologicalNodeAbstract n : nodes){
-			if(neighbors.get(n.getID()).size()<3){
-				subgraphs.remove(n.getID());
-				continue;
-			}
-			somethingChanged= true;
-			neigh = neighbors.get(n.getID());
-			sortedNodes = new HashSet<BiologicalNodeAbstract>();
-			while(somethingChanged){
-				if(subgraphs.get(n.getID()).size()<2){
-					break;
-				}
-				somethingChanged = false;
-				theSubgraphs = new ArrayList<List<BiologicalNodeAbstract>>();
-				theSubgraphs.addAll(subgraphs.get(n.getID()));
-				for(List<BiologicalNodeAbstract> sg : theSubgraphs){
-					boolean somethingChangedInThisSubgraph = false;
-					HashSet<BiologicalNodeAbstract> newNodeCopy = new HashSet<BiologicalNodeAbstract>();
-					newNodeCopy.addAll(sg);
-					for(BiologicalNodeAbstract newNode : newNodeCopy){
-							HashSet<BiologicalNodeAbstract> newElements = new HashSet<BiologicalNodeAbstract>();
-							newElements.addAll(neighbors.get(newNode.getID()));
-							newElements.remove(n);
-							newElements.removeIf(p -> sg.contains(p));
-							retainCheck = new HashSet<BiologicalNodeAbstract>();
-							retainCheck.addAll(newElements);
-							retainCheck.retainAll(sortedNodes);
-							if(retainCheck.size()!=0){
-								sortedNodes.removeAll(sg);
-								subgraphs.get(n.getID()).remove(sg);
-								somethingChanged = true;
-								somethingChangedInThisSubgraph = true;
-								break;
-							}
-							if(!newElements.isEmpty()){
-								sg.addAll(newElements);
-								sortedNodes.add(newNode);
-								somethingChangedInThisSubgraph = true;
-								somethingChanged = true;
-							}
-					}
-					// If only 2 subgraphs left and one of them is not changing any more, we're done.
-					if(!somethingChangedInThisSubgraph && subgraphs.get(n.getID()).size()==2){
-						List<BiologicalNodeAbstract> othersg = theSubgraphs.get(0);
-						if(othersg==sg){
-							othersg = theSubgraphs.get(1);
-						}
-						othersg.clear();
-						othersg.addAll(nodes);
-						othersg.remove(n);
-						othersg.removeAll(sg);
-						somethingChanged = false;
-						break;
-					}
-				}
-			}
-			if(subgraphs.get(n.getID()).size()<2){
-				subgraphs.remove(n.getID());
-			} else {
-				List<BiologicalNodeAbstract> largestList = new ArrayList<BiologicalNodeAbstract>();
-				for(List<BiologicalNodeAbstract> l : subgraphs.get(n.getID())){
-					if(l.size()>largestList.size()){
-						largestList = l;
-					}
-				}
-				subgraphs.get(n.getID()).remove(largestList);
-			}
-		}
-		System.out.println("done1");
-		
-//		List<BiologicalNodeAbstract> largestList = new ArrayList<BiologicalNodeAbstract>();
-//		for(Integer key : subgraphs.keySet()){
-//			largestList = new ArrayList<BiologicalNodeAbstract>();
-//			for(List<BiologicalNodeAbstract> l : subgraphs.get(key)){
-//				if(l.size()>largestList.size()){
-//					largestList = l;
-//				}
-//			}
-//			subgraphs.get(key).remove(largestList);
+//	public static void coarseSeperatedSubgraphs(Pathway pw){
+//		
+//		// Take only inner nodes.
+//		Set<BiologicalNodeAbstract> nodes = new HashSet<BiologicalNodeAbstract>();
+//		
+//		if(pw.isBNA()){
+//			nodes.addAll(((BiologicalNodeAbstract) pw).getInnerNodes());
+//		} else {
+//			nodes.addAll(pw.getAllNodes());
 //		}
-		
-//		for(BiologicalNodeAbstract node : nodes){
-//			if(subgraphs.keySet().contains(node.getID())){
-//				System.out.println(node.getLabel() + ":");
-//				for(List<BiologicalNodeAbstract> l : subgraphs.get(node.getID())){
-//					for(BiologicalNodeAbstract nd : l){
-//						System.out.print(nd.getLabel() + ",");
+//		
+//		// Initialisation of neighbors and Subgraph lists.
+//		HashMap<Integer, List<List<BiologicalNodeAbstract>>> subgraphs = new HashMap<Integer, List<List<BiologicalNodeAbstract>>>();
+//		List<BiologicalNodeAbstract> subgraph = new ArrayList<BiologicalNodeAbstract>();
+//		List<List<BiologicalNodeAbstract>> theSubgraphs = new ArrayList<List<BiologicalNodeAbstract>>();
+//		HashMap<Integer, List<BiologicalNodeAbstract>> neighbors = new HashMap<Integer, List<BiologicalNodeAbstract>>();
+//		List<BiologicalNodeAbstract> neigh = new ArrayList<BiologicalNodeAbstract>();
+//		
+//		for(BiologicalNodeAbstract n : nodes){
+//			neigh = new ArrayList<BiologicalNodeAbstract>();
+//			neigh.addAll(pw.getGraph().getJungGraph().getNeighbors(n));
+//			// delete environment nodes.
+//			neigh.retainAll(nodes);
+//			neighbors.put(n.getID(), neigh);
+//			theSubgraphs = new ArrayList<List<BiologicalNodeAbstract>>();
+//			for(BiologicalNodeAbstract neighbor : neigh){
+//				subgraph = new ArrayList<BiologicalNodeAbstract>();
+//				subgraph.add(neighbor);
+//				theSubgraphs.add(subgraph);
+//			}
+//			subgraphs.put(n.getID(),theSubgraphs);
+//		}
+//		
+//		// Compute seperating nodes with more than 2 neighbors
+//		HashSet<BiologicalNodeAbstract> sortedNodes = new HashSet<BiologicalNodeAbstract>();
+//		HashSet<BiologicalNodeAbstract> retainCheck = new HashSet<BiologicalNodeAbstract>();
+//		boolean somethingChanged= true;
+//		for(BiologicalNodeAbstract n : nodes){
+//			if(neighbors.get(n.getID()).size()<3){
+//				subgraphs.remove(n.getID());
+//				continue;
+//			}
+//			somethingChanged= true;
+//			neigh = neighbors.get(n.getID());
+//			sortedNodes = new HashSet<BiologicalNodeAbstract>();
+//			while(somethingChanged){
+//				if(subgraphs.get(n.getID()).size()<2){
+//					break;
+//				}
+//				somethingChanged = false;
+//				theSubgraphs = new ArrayList<List<BiologicalNodeAbstract>>();
+//				theSubgraphs.addAll(subgraphs.get(n.getID()));
+//				for(List<BiologicalNodeAbstract> sg : theSubgraphs){
+//					boolean somethingChangedInThisSubgraph = false;
+//					HashSet<BiologicalNodeAbstract> newNodeCopy = new HashSet<BiologicalNodeAbstract>();
+//					newNodeCopy.addAll(sg);
+//					for(BiologicalNodeAbstract newNode : newNodeCopy){
+//							HashSet<BiologicalNodeAbstract> newElements = new HashSet<BiologicalNodeAbstract>();
+//							newElements.addAll(neighbors.get(newNode.getID()));
+//							newElements.remove(n);
+//							newElements.removeIf(p -> sg.contains(p));
+//							retainCheck = new HashSet<BiologicalNodeAbstract>();
+//							retainCheck.addAll(newElements);
+//							retainCheck.retainAll(sortedNodes);
+//							if(retainCheck.size()!=0){
+//								sortedNodes.removeAll(sg);
+//								subgraphs.get(n.getID()).remove(sg);
+//								somethingChanged = true;
+//								somethingChangedInThisSubgraph = true;
+//								break;
+//							}
+//							if(!newElements.isEmpty()){
+//								sg.addAll(newElements);
+//								sortedNodes.add(newNode);
+//								somethingChangedInThisSubgraph = true;
+//								somethingChanged = true;
+//							}
 //					}
-//				System.out.println();
+//					// If only 2 subgraphs left and one of them is not changing any more, we're done.
+//					if(!somethingChangedInThisSubgraph && subgraphs.get(n.getID()).size()==2){
+//						List<BiologicalNodeAbstract> othersg = theSubgraphs.get(0);
+//						if(othersg==sg){
+//							othersg = theSubgraphs.get(1);
+//						}
+//						othersg.clear();
+//						othersg.addAll(nodes);
+//						othersg.remove(n);
+//						othersg.removeAll(sg);
+//						somethingChanged = false;
+//						break;
+//					}
 //				}
-//			System.out.println();
+//			}
+//			if(subgraphs.get(n.getID()).size()<2){
+//				subgraphs.remove(n.getID());
+//			} else {
+//				List<BiologicalNodeAbstract> largestList = new ArrayList<BiologicalNodeAbstract>();
+//				for(List<BiologicalNodeAbstract> l : subgraphs.get(n.getID())){
+//					if(l.size()>largestList.size()){
+//						largestList = l;
+//					}
+//				}
+//				subgraphs.get(n.getID()).remove(largestList);
 //			}
 //		}
+//		System.out.println("done1");
+//		
+////		List<BiologicalNodeAbstract> largestList = new ArrayList<BiologicalNodeAbstract>();
+////		for(Integer key : subgraphs.keySet()){
+////			largestList = new ArrayList<BiologicalNodeAbstract>();
+////			for(List<BiologicalNodeAbstract> l : subgraphs.get(key)){
+////				if(l.size()>largestList.size()){
+////					largestList = l;
+////				}
+////			}
+////			subgraphs.get(key).remove(largestList);
+////		}
+//		
+////		for(BiologicalNodeAbstract node : nodes){
+////			if(subgraphs.keySet().contains(node.getID())){
+////				System.out.println(node.getLabel() + ":");
+////				for(List<BiologicalNodeAbstract> l : subgraphs.get(node.getID())){
+////					for(BiologicalNodeAbstract nd : l){
+////						System.out.print(nd.getLabel() + ",");
+////					}
+////				System.out.println();
+////				}
+////			System.out.println();
+////			}
+////		}
+//		
+//		class HLC implements HierarchyListComparator<Integer> {
+//			
+//			public HLC() {
+//			}
+//
+//			public Integer getValue(BiologicalNodeAbstract n) {
+//				Integer currentSize = Integer.MAX_VALUE;
+//				Integer parentID = n.getID();
+//				for(Integer key : subgraphs.keySet()){
+//					for(List<BiologicalNodeAbstract> l : subgraphs.get(key)){
+//						if(l.contains(n)){
+//							if(l.size()<currentSize){
+//								currentSize = l.size();
+//								parentID = key;
+//							}
+//						break;
+//						}
+//					}
+//				}
+//				return parentID;
+//			}
+//
+//			public Integer getSubValue(BiologicalNodeAbstract n) {
+//				return n.getID();
+//			}
+//		}
+//
+//		HierarchyList<Integer> l = new HierarchyList<Integer>();
+//		l.addAll(nodes);
+//		l.sort(new HLC());
+//		System.out.println("done2");
+//		l.coarse();
+//		System.out.println("done3");
+//		
+//	}
+	
+	
+public static void coarseSeperatedSubgraphs(Pathway pw){
 		
+		// Build Graph Analysis Tree
+		long timeStart = System.currentTimeMillis();
+		GraphAnalysisTree gat = new GraphAnalysisTree(pw);
+		gat.build();
+		gat.getSplittingNodes();
+		long timeEnd = System.currentTimeMillis();
+		System.out.println("Runtime: " + (timeEnd-timeStart) + " millis");
+		HashMap<BiologicalNodeAbstract, Collection<Set<BiologicalNodeAbstract>>> map = gat.getSplittingNodes();
+
+		for(BiologicalNodeAbstract key : map.keySet()){
+			int maxSize = 0;
+			Set<BiologicalNodeAbstract> toBeRemoved = null;
+			for(Set<BiologicalNodeAbstract> l : map.get(key)){
+				if(l.size()>maxSize){
+					toBeRemoved = l;
+					maxSize = l.size();
+				}
+			}
+			for(Set<BiologicalNodeAbstract> l : map.get(key)){
+				l.removeAll(toBeRemoved);
+			}
+		}
 		class HLC implements HierarchyListComparator<Integer> {
 			
 			public HLC() {
@@ -152,12 +211,12 @@ public class AutoCoarse {
 			public Integer getValue(BiologicalNodeAbstract n) {
 				Integer currentSize = Integer.MAX_VALUE;
 				Integer parentID = n.getID();
-				for(Integer key : subgraphs.keySet()){
-					for(List<BiologicalNodeAbstract> l : subgraphs.get(key)){
+				for(BiologicalNodeAbstract key : map.keySet()){
+					for(Set<BiologicalNodeAbstract> l : map.get(key)){
 						if(l.contains(n)){
 							if(l.size()<currentSize){
 								currentSize = l.size();
-								parentID = key;
+								parentID = key.getID();
 							}
 						break;
 						}
@@ -172,11 +231,10 @@ public class AutoCoarse {
 		}
 
 		HierarchyList<Integer> l = new HierarchyList<Integer>();
-		l.addAll(nodes);
+		l.addAll(pw.getAllNodes());
 		l.sort(new HLC());
-		System.out.println("done2");
 		l.coarse();
-		System.out.println("done3");
-		
+		timeEnd = System.currentTimeMillis();
+		System.out.println("Runtime: " + (timeEnd-timeStart) + " millis");		
 	}
 }
