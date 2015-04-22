304d7250efa40a20702ce80a6d6ffe58fc2f2913
mlewinsk <mlewinsk@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2015-04-22 16:04:35 +0000

    Multidimensional scaling now works with text attributes like GO-Terms, too.
    For testing on an UNID Pathway: new MultidimensionalScaling(NodeAttributeNames.GO_CELLULAR_COMPONENT_,true);

--- VANESA2/src/graph/algorithms/MultidimensionalScaling.java
+++ VANESA2/src/graph/algorithms/MultidimensionalScaling.java
@@ -2,7 +2,10 @@ package graph.algorithms;
 
 import java.awt.Point;
 import java.util.*;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
+
+import org.jfree.chart.JFreeChart;
 
 import graph.ContainerSingelton;
 import graph.GraphContainer;
@@ -19,16 +22,25 @@ public class MultidimensionalScaling {
 	private GraphContainer con = ContainerSingelton.getInstance();
 	private Pathway pw;
 	private MyGraph mg;
-	private int nodes, dimensions = 2, scaling = 1024; //scaling in pixels x*y / 1000*1000
+	private int nodes, dimensions = 2, scaling = 1024; // scaling in pixels x*y
+														// / 1000*1000
 	double[][] dissim, output;
 	double[] mins, maxes;
 	ArrayList<Double> expvalues;
+	ArrayList<String> txtvalues;
 
 	Hashtable<BiologicalNodeAbstract, Integer> nodeassings = new Hashtable<BiologicalNodeAbstract, Integer>();
 	Hashtable<Integer, BiologicalNodeAbstract> nodeassignsback = new Hashtable<Integer, BiologicalNodeAbstract>();
 	private int nodeincrement = 0;
 
-	public MultidimensionalScaling(String nodeAttributeName) {
+	/**
+	 * Constructor for Node-attribute data
+	 * 
+	 * @param nodeAttributeName
+	 *            - Name of the attribute Name having double value attributes
+	 * @param string - true -> nodeattributes are text based, false -> attributes are numbers(doubles) 
+	 */
+	public MultidimensionalScaling(String nodeAttributeName, boolean string) {
 
 		pw = con.getPathway(w.getCurrentPathway());
 		mg = pw.getGraph();
@@ -36,125 +48,173 @@ public class MultidimensionalScaling {
 		nodes = mg.getAllVertices().size();
 
 		dissim = new double[nodes][nodes];
-		expvalues = new ArrayList<>(nodes);
 
-		NodeAttribute attribute;
+		if (!string) {
+			expvalues = new ArrayList<>(nodes);
 
-		// Induce mapping to local adjacency matrix and data structures (BNA.ID)
-		Iterator<BiologicalNodeAbstract> it = mg.getAllVertices().iterator();
-		while (it.hasNext()) {
-			BiologicalNodeAbstract bna = it.next();
-			reassignNodeBNA(bna);
-			attribute = bna.getNodeAttributeByName(nodeAttributeName);
-			if (attribute != null) {
-				expvalues.add(attribute.getDoublevalue());
-			} else {
-				expvalues.add(Math.random()/33.0d);
-			}
-		}
+			NodeAttribute attribute;
 
-//		System.out.println(expvalues);
-
-		DoubleSummaryStatistics stats = expvalues.stream().collect(
-				Collectors.summarizingDouble(Double::doubleValue));
-
-//		System.out.println(stats);
-
-		// calcdissim
-		for (int i = 0; i < dissim.length; i++) {
-			for (int j = i + 1; j < dissim.length; j++) {
-				dissim[i][j] = Math.sqrt(getDissimilarity(expvalues.get(i),
-						expvalues.get(j), stats.getMin()));
-				dissim[j][i] = dissim[i][j];
-			}
-		}
-
-		// DEBUG
-//		printDissimilarityMatrix();
-		System.out.println();
-
-//		output = MDSJ.classicalScaling(dissim, dimensions);
-		output = MDSJ.stressMinimization(dissim, dimensions);
-
-		
-//		printOutputMatrix();
-
-		
-		mins = new double[dimensions];
-		maxes = new double[dimensions];
-		
-		Arrays.setAll(mins, d -> Double.MAX_VALUE);
-		Arrays.setAll(maxes, d -> Double.MIN_VALUE);
-		
-		double currentdouble;
-		//get min and max dimensions
-		for(int x = 0; x < dimensions; x++){
-			for(int y = 0; y < nodes; y++){
-				currentdouble = output[x][y] ;
-				if(currentdouble > maxes[x]){
-					maxes[x] = currentdouble  ;
-				}
-				if(currentdouble  < mins[x]){
-					mins[x] = currentdouble  ;
+			// Induce mapping to local adjacency matrix and data structures
+			// (BNA.ID)
+			Iterator<BiologicalNodeAbstract> it = mg.getAllVertices()
+					.iterator();
+			while (it.hasNext()) {
+				BiologicalNodeAbstract bna = it.next();
+				reassignNodeBNA(bna);
+				attribute = bna.getNodeAttributeByName(nodeAttributeName);
+				if (attribute != null) {
+					expvalues.add(attribute.getDoublevalue());
+				} else {
+					expvalues.add(Math.random() / 33.0d);
 				}
 			}
+
+			// System.out.println(expvalues);
+			DoubleSummaryStatistics stats = expvalues.stream().collect(
+					Collectors.summarizingDouble(Double::doubleValue));
+			// System.out.println(stats);
+			// calcdissim
+			for (int i = 0; i < dissim.length; i++) {
+				for (int j = i + 1; j < dissim.length; j++) {
+					dissim[i][j] = Math.sqrt(getDissimilarity(expvalues.get(i),
+							expvalues.get(j), stats.getMin()));
+					dissim[j][i] = dissim[i][j];
+				}
+			}
+			// DEBUG
+			// printDissimilarityMatrix();
+			// output = MDSJ.classicalScaling(dissim, dimensions);
+			output = MDSJ.stressMinimization(dissim, dimensions);
+			// printOutputMatrix();
+
+			mins = new double[dimensions];
+			maxes = new double[dimensions];
+			Arrays.setAll(mins, d -> Double.MAX_VALUE);
+			Arrays.setAll(maxes, d -> Double.MIN_VALUE);
+			double currentdouble;
+			// get min and max dimensions
+			for (int x = 0; x < dimensions; x++) {
+				for (int y = 0; y < nodes; y++) {
+					currentdouble = output[x][y];
+					if (currentdouble > maxes[x]) {
+						maxes[x] = currentdouble;
+					}
+					if (currentdouble < mins[x]) {
+						mins[x] = currentdouble;
+					}
+				}
+			}
+
+			// DEBUG
+			// System.out.println(Arrays.toString(mins));
+			// System.out.println(Arrays.toString(maxes));
+
+			
+			//MDS is textbased
+		} else {
+			
+			//determine statistics
+			txtvalues = new ArrayList<>(nodes);
+			HashMap<String,Integer> occur = new HashMap<>(nodes);
+
+			Iterator<BiologicalNodeAbstract> it = mg.getAllVertices()
+					.iterator();
+			while (it.hasNext()) {
+				BiologicalNodeAbstract bna = it.next();
+				reassignNodeBNA(bna);
+				for(NodeAttribute na : bna.getNodeAttributes()){
+
+					if(na.getName().equals(nodeAttributeName)){
+						if(!occur.containsKey(na.getStringvalue()))
+							occur.put(na.getStringvalue(), 1);
+						else
+							occur.put(na.getStringvalue(), occur.get(na.getStringvalue())+1);
+					}
+				}
+			}
+			
+			System.out.println(entriesSortedByValues(occur));
+
+			//display sorted values
+			
+			
+			ArrayList<BiologicalNodeAbstract> nodes = new ArrayList<>();
+			nodes.addAll(mg.getAllVertices());
+			for (int i = 0; i < nodes.size(); i++) {
+				BiologicalNodeAbstract bnai = nodes.get(i);
+				for (int j = i+1; j < nodes.size(); j++) {
+					BiologicalNodeAbstract bnaj = nodes.get(j);
+					for(NodeAttribute na : bnai.getNodeAttributes()){
+						for (NodeAttribute nb : bnaj.getNodeAttributes()) {
+							if(na.getName().equals(nodeAttributeName)){
+								if(nb.getName().equals(nodeAttributeName)){
+									if(na.getStringvalue().equals(nb.getStringvalue())){
+									dissim[i][j]+= (Math.random()/10.0d)+1; 
+									dissim[j][i] = dissim[i][j]; 
+									}
+								}	
+							}	
+						}					
+					}
+				}
+			}
+
+			output = MDSJ.stressMinimization(dissim, dimensions);
+					
+
 		}
-		
-		//DEBUG
-//		System.out.println(Arrays.toString(mins));
-//		System.out.println(Arrays.toString(maxes));
-		
-		
 		realignNetwork();
 	}
+	
 
 	private void realignNetwork() {
-		
-		//scale
+
+		// scale
 		for (int i = 0; i < nodes; i++) {
-			output[0][i] = (output[0][i])*scaling;
-			output[1][i]= (output[1][i])*scaling;
+			output[0][i] = (output[0][i]) * scaling;
+			output[1][i] = (output[1][i]) * scaling;
 		}
-//		System.out.println("--- trans ---");
-//		printOutputMatrix();
-		
-		//check for multiple nodes on same coordinates
+		// System.out.println("--- trans ---");
+		// printOutputMatrix();
+
+		// check for multiple nodes on same coordinates
 		int duplicatecoords = 0, x1, x2, y1, y2;
 		for (int i = 0; i < nodes; i++) {
-			for (int j = i+1; j < nodes; j++) {
+			for (int j = i + 1; j < nodes; j++) {
 				x1 = (int) output[0][i];
 				x2 = (int) output[0][j];
-				
+
 				y1 = (int) output[1][i];
 				y2 = (int) output[1][j];
-				
-				if(x1 == x2 && y1 == y2)
-					duplicatecoords++;				
-			}			
-		}		
-//		System.out.println("piled nodes "+duplicatecoords);
-		
-		
+
+				if (x1 == x2 && y1 == y2)
+					duplicatecoords++;
+			}
+		}
+		// System.out.println("piled nodes "+duplicatecoords);
+
 		for (int node = 0; node < nodes; node++) {
 			mg.getVisualizationViewer()
 					.getModel()
 					.getGraphLayout()
 					.setLocation(
 							nodeassignsback.get(node),
-							new Point((int) (output[0][node]+1.5),
-									(int) (output[1][node]+1.5)));
+							new Point((int) (output[0][node] + 1.5),
+									(int) (output[1][node] + 1.5)));
 		}
 	}
 
 	private double getDissimilarity(Double d1, Double d2, Double min) {
-//		return (Double.max(d1, d2) + min) - (Double.min(d1, d2) + min);
-		//EUCL
-		return Math.sqrt(Math.pow((Double.max(d1, d2) + min) - (Double.min(d1, d2) + min),2));
-//System.out.println(Math.pow(8,1.0/3.0));
-		
-		
-//		return Math.pow(Math.pow((Double.max(d1, d2) + min) - (Double.min(d1, d2) + min),12),1.0/12.0);
-//		return Math.sqrt((Double.max(d1, d2) + min) - (Double.min(d1, d2) + min));
+		// return (Double.max(d1, d2) + min) - (Double.min(d1, d2) + min);
+		// EUCL
+		return Math.sqrt(Math.pow(
+				(Double.max(d1, d2) + min) - (Double.min(d1, d2) + min), 2));
+		// System.out.println(Math.pow(8,1.0/3.0));
+
+		// return Math.pow(Math.pow((Double.max(d1, d2) + min) - (Double.min(d1,
+		// d2) + min),12),1.0/12.0);
+		// return Math.sqrt((Double.max(d1, d2) + min) - (Double.min(d1, d2) +
+		// min));
 
 	}
 
@@ -183,4 +243,26 @@ public class MultidimensionalScaling {
 			nodeincrement++;
 		}
 	}
+
+	/**
+	 * Sort method for values
+	 * @param map
+	 * @return
+	 */
+	static <K, V extends Comparable<? super V>> List<Entry<K, V>> entriesSortedByValues(
+			Map<K, V> map) {
+
+		List<Entry<K, V>> sortedEntries = new ArrayList<Entry<K, V>>(
+				map.entrySet());
+
+		Collections.sort(sortedEntries, new Comparator<Entry<K, V>>() {
+			@Override
+			public int compare(Entry<K, V> e1, Entry<K, V> e2) {
+				return e2.getValue().compareTo(e1.getValue());
+			}
+		});
+
+		return sortedEntries;
+	}
+
 }
