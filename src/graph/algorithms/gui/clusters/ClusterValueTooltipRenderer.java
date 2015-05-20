2dba3d7527cfabbc8836a280c1ab354ac9f019c8
mlewinsk <mlewinsk@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2015-05-20 10:34:29 +0000

    Improved Clusterview with own Tooltip

---
+++ src/graph/algorithms/gui/clusters/ClusterValueTooltipRenderer.java
@@ -0,0 +1,47 @@
+package graph.algorithms.gui.clusters;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+import java.awt.Color;
+import java.awt.Component;
+
+/**
+ * 
+ * Custom renderer to show experimental values in cluster's cell tooltip
+ * 
+ * @author mlewinsk May 2015
+ */
+public class ClusterValueTooltipRenderer extends JLabel implements
+		TableCellRenderer {
+	/**
+	 * generated UID
+	 */
+	private static final long serialVersionUID = -8026637709290430471L;
+
+	private String[] values;
+
+	public ClusterValueTooltipRenderer(String[] values) {
+		this.values = values;
+
+		setOpaque(true); // MUST do this for background to show up.
+	}
+
+	public Component getTableCellRendererComponent(JTable table,
+			Object treeset, boolean isSelected, boolean hasFocus, int row,
+			int column) {
+
+		if (isSelected) {
+			setBackground(table.getSelectionBackground());
+		} else {
+			setBackground(table.getBackground());
+		}
+
+		setToolTipText(values[row]);
+		setText(treeset.toString());
+
+		return this;
+	}
+}
