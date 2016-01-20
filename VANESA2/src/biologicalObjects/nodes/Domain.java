4b58597af569a972ea1a7e3892107484d7d48a22
cbrinkro <cbrinkro@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2016-01-20 14:24:50 +0000

    - example files which contains all possible nodes / edges - fix of several NPEs

--- VANESA2/src/biologicalObjects/nodes/Domain.java
+++ VANESA2/src/biologicalObjects/nodes/Domain.java
@@ -3,15 +3,15 @@ package biologicalObjects.nodes;
 import graph.jung.graphDrawing.VertexShapes;
 import biologicalElements.Elementdeclerations;
 
-public class Complex extends BiologicalNodeAbstract{
+public class Domain extends BiologicalNodeAbstract{
 	
-	public Complex(String label, String name) {
+	public Domain(String label, String name) {
 		super(label, name);
-		setBiologicalElement(Elementdeclerations.complex);
+		setBiologicalElement(Elementdeclerations.domain);
 		shapes = new VertexShapes();
 		attributeSetter(this.getClass().getSimpleName(), this);
 	}
-
+	
 	@Override
 	public void rebuildShape(VertexShapes vs){
 		//setShape(shapes.getRegularStar(getVertex(), 6));
