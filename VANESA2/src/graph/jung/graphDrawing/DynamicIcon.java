c247c5e1242a5623204a2c92d48bfc109d3b1cef
cbrinkro <cbrinkro@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2016-11-10 18:11:04 +0000

    debug commit

--- VANESA2/src/graph/jung/graphDrawing/DynamicIcon.java
+++ VANESA2/src/graph/jung/graphDrawing/DynamicIcon.java
@@ -1,7 +1,14 @@
 package graph.jung.graphDrawing;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.Icon;
 
@@ -24,6 +31,8 @@ public class DynamicIcon implements Icon {
 	}
 
 	public void paintIcon(Component c, Graphics g, int x, int y) {
+		
+		Graphics2D g2 = (Graphics2D) g;
 		// g.fill3DRect(x, y, getIconWidth(), getIconHeight(), true);
 		// g.setColor(new Color(0,0,0));
 		// g.drawRect(x, y, 20, 30);
@@ -76,7 +85,16 @@ public class DynamicIcon implements Icon {
 			xpos = new Double(x - x1 + 21 - 5 * ((double) tokens.length() / 2))
 			.intValue();
 		}
-		g.drawString(tokens, xpos, y + 7);
+		
+		g.setColor(Color.BLACK);
+		
+		Shape s = p.getShape();
+		s.getBounds2D().setFrame(xpos,y,50, 50);
+		g2.draw(s);
+		//g2.draw(AffineTransform.getScaleInstance(p.getNodesize(), p.getNodesize()).createTransformedShape(s));
+   	 	
+   	//g2.draw(s);
+		g2.drawString(tokens, xpos, y + 7);
 
 		// }
 	}
