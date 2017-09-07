1ac70f42469dc8cb8839b05506b7b61bd317d933
cbrinkro <cbrinkro@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2017-09-07 16:36:01 +0000

    - bar as arrow head for inhibition arcs

--- VANESA2/src/graph/jung/graphDrawing/MyEdgeArrowFunction.java
+++ VANESA2/src/graph/jung/graphDrawing/MyEdgeArrowFunction.java
@@ -2,6 +2,7 @@ package graph.jung.graphDrawing;
 
 import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.GeneralPath;
 
 import org.apache.commons.collections15.Transformer;
 
@@ -18,22 +19,32 @@ import edu.uci.ics.jung.visualization.decorators.DirectionalEdgeArrowTransformer
  * 
  */
 public class MyEdgeArrowFunction
-		implements
-		Transformer<Context<Graph<BiologicalNodeAbstract, BiologicalEdgeAbstract>, BiologicalEdgeAbstract>, Shape> {
+		implements Transformer<Context<Graph<BiologicalNodeAbstract, BiologicalEdgeAbstract>, BiologicalEdgeAbstract>, Shape> {
 
 	@Override
-	public Shape transform(
-			Context<Graph<BiologicalNodeAbstract, BiologicalEdgeAbstract>, BiologicalEdgeAbstract> context) {
-		//System.out.println(context.element.getBiologicalElement());
-		if ((context.element.getBiologicalElement().equals(
-				biologicalElements.Elementdeclerations.pnInhibitionEdge)
-				|| context.element.getBiologicalElement().equals(
-						biologicalElements.Elementdeclerations.inhibitionEdge) || context.element
-				.getBiologicalElement().equals(
-						biologicalElements.Elementdeclerations.inhibitor))) {
+	public Shape transform(Context<Graph<BiologicalNodeAbstract, BiologicalEdgeAbstract>, BiologicalEdgeAbstract> context) {
+		// System.out.println(context.element.getBiologicalElement());
+		// System.out.println("element:
+		// "+context.element.getBiologicalElement());
+		if (context.element.getBiologicalElement().equals(biologicalElements.Elementdeclerations.pnInhibitionEdge)) {
+
 			return new Ellipse2D.Double(-10, -5, 10, 10);
+		} else if (context.element.getBiologicalElement().equals(biologicalElements.Elementdeclerations.inhibitionEdge)
+				|| context.element.getBiologicalElement().equals(biologicalElements.Elementdeclerations.inhibitor)) {
+			return this.getInhibitoryArrowHead(3, 30, -3);
+
 		}
-		return new DirectionalEdgeArrowTransformer<BiologicalNodeAbstract, BiologicalEdgeAbstract>(
-				10, 8, 4).transform(context);
+		return new DirectionalEdgeArrowTransformer<BiologicalNodeAbstract, BiologicalEdgeAbstract>(10, 8, 4).transform(context);
+	}
+
+	private Shape getInhibitoryArrowHead(float width, float length, float offset) {
+		GeneralPath arrow = new GeneralPath();
+		arrow.moveTo(offset, 0);
+		arrow.lineTo(offset, length / 2.0f);
+		arrow.lineTo(offset - width, length / 2.0f);
+		arrow.lineTo(offset - width, -length / 2.0f);
+		arrow.lineTo(offset, -length / 2.0f);
+		arrow.lineTo(offset, 0);
+		return arrow;
 	}
 }
