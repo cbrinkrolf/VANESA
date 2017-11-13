c63f41330c2a07c94a7fb6e198b5df2da571857a
cbrinkro <christoph.brinkrolf@uni-bielefeld.de>
2017-11-13 17:25:50 +0000

    - nodes can be continuous / discrete - inhibition can be absolute or relative

--- src/biologicalObjects/edges/Inhibition.java
+++ src/biologicalObjects/edges/Inhibition.java
@@ -5,11 +5,20 @@ import biologicalElements.Elementdeclerations;
 import biologicalObjects.nodes.BiologicalNodeAbstract;
 
 public class Inhibition extends BiologicalEdgeAbstract{
+	
+	private boolean absoluteInhibition = true;
 
 	public Inhibition(String label, String name, BiologicalNodeAbstract from, BiologicalNodeAbstract to) {
 		super(label, name, from, to);
 		setBiologicalElement(Elementdeclerations.inhibitionEdge);
 	}
 
+	public boolean isAbsoluteInhibition() {
+		return absoluteInhibition;
+	}
+
+	public void setAbsoluteInhibition(boolean absoluteInhibition) {
+		this.absoluteInhibition = absoluteInhibition;
+	}
 	
 }
