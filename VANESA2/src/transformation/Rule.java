ea074771a3608a70d5def6a2fb173293938263ad
cbrinkro <cbrinkro@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2018-04-11 18:26:25 +0000

    little stuff

---
+++ VANESA2/src/transformation/Rule.java
@@ -0,0 +1,17 @@
+package transformation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rule {
+	
+	private List<String> nodeNames = new ArrayList<String>();
+	private List<String> nodeTypes = new ArrayList<String>();
+	
+	private List<String> edgeNames = new ArrayList<String>();
+	private List<String> edgeTypes = new ArrayList<String>();
+	private List<String> edgeFrom = new ArrayList<String>();
+	private List<String> edgeTo = new ArrayList<String>();
+	
+
+}
