969ad6f868a39136b29b8ae8c00402e84c0f3f67
cbrinkro <cbrinkro@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2018-05-18 22:03:38 +0000

    little more stuff for transformation

--- VANESA2/src/transformation/Rule.java
+++ VANESA2/src/transformation/Rule.java
@@ -13,5 +13,28 @@ public class Rule {
 	private List<String> edgeFrom = new ArrayList<String>();
 	private List<String> edgeTo = new ArrayList<String>();
 	
+	
+	
+	public List<String> getNodeNames() {
+		return nodeNames;
+	}
+	public List<String> getNodeTypes() {
+		return nodeTypes;
+	}
+	public List<String> getEdgeNames() {
+		return edgeNames;
+	}
+	public List<String> getEdgeTypes() {
+		return edgeTypes;
+	}
+	public List<String> getEdgeFrom() {
+		return edgeFrom;
+	}
+	public List<String> getEdgeTo() {
+		return edgeTo;
+	}
+	
+	
+	
 
 }
