9132ac2920e9e60729fda46cf1082c941a04854e
j-seidel <j-seidel@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2019-07-11 12:52:39 +0000

    Add missing files

---
+++ src/graph/groups/Group.java
@@ -0,0 +1,15 @@
+package graph.groups;
+
+import biologicalObjects.nodes.BiologicalNodeAbstract;
+
+import java.util.ArrayList;
+
+public class Group {
+
+    public ArrayList<BiologicalNodeAbstract> nodes;
+
+    public Group(ArrayList<BiologicalNodeAbstract> nodes ) {
+        this.nodes = nodes;
+
+    }
+}
