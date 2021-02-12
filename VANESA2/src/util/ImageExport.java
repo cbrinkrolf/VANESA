dd3f068dedd6b594b1fa156b4624ec4a91d35a1c
cbrinkro <cbrinkro@6b122553-c3aa-43c9-b343-39c6dc6d5d3d>
2021-02-12 08:25:22 +0000

    - remove clipPaths in generated SVG which might clip longer text

--- VANESA2/src/util/ImageExport.java
+++ VANESA2/src/util/ImageExport.java
@@ -28,141 +28,170 @@ import org.apache.batik.anim.dom.SVGDOMImplementation;
 import org.apache.batik.svggen.SVGGraphics2D;
 import org.apache.batik.transcoder.TranscoderException;
 import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.w3c.dom.svg.SVGDocument;
+import org.w3c.dom.svg.SVGElement;
+import org.w3c.dom.svg.SVGSVGElement;
 
 /**
- * Export to figure formats for Components.
- * Created by Ken on 12/1/2017.
+ * Export to figure formats for Components. Created by Ken on 12/1/2017.
  * https://github.com/wenbostar/PDV/blob/master/src/main/java/PDVGUI/utils/Export.java
  */
 
-
-
-
 public class ImageExport {
-	
+
 	public static final String IMAGE_TYPE_SVG = "svg";
 	public static final String IMAGE_TYPE_PNG = "png";
 
-    /**
-     * Constructor
-     * @param component Component to export
-     * @param bounds Dimensions of the viewport
-     * @param exportFile Output file
-     * @param imageType Image type
-     */
-    public static void exportPic(Component component, Rectangle bounds, File exportFile, String imageType) throws IOException, TranscoderException {
+	/**
+	 * Constructor
+	 * 
+	 * @param component  Component to export
+	 * @param bounds     Dimensions of the viewport
+	 * @param exportFile Output file
+	 * @param imageType  Image type
+	 */
+	public static void exportPic(Component component, Rectangle bounds, File exportFile, String imageType)
+			throws IOException, TranscoderException {
 
-        if (imageType == IMAGE_TYPE_PNG){
-            exportPNG(component, bounds, exportFile);
-        } else if (imageType == IMAGE_TYPE_SVG){
-            exportSVG(component, bounds, exportFile);
-        }
-    }
+		if (imageType == IMAGE_TYPE_PNG) {
+			exportPNG(component, bounds, exportFile);
+		} else if (imageType == IMAGE_TYPE_SVG) {
+			exportSVG(component, bounds, exportFile);
+		}
+	}
 
-    /**
-     * Export component to pdf format
-     * @param component Component
-     * @param bounds Rectangle
-     * @param exportFile Export file
-     * @throws IOException
-     * @throws TranscoderException
-     */
-    private static void exportSVG(Component component, Rectangle bounds, File exportFile) throws IOException, TranscoderException {
+	/**
+	 * Export component to pdf format
+	 * 
+	 * @param component  Component
+	 * @param bounds     Rectangle
+	 * @param exportFile Export file
+	 * @throws IOException
+	 * @throws TranscoderException
+	 */
+	private static void exportSVG(Component component, Rectangle bounds, File exportFile)
+			throws IOException, TranscoderException {
 
-        DOMImplementation domImplementation = SVGDOMImplementation.getDOMImplementation();
-        String svgNS = "http://www.w3.org/2000/svg";
-        SVGDocument svgDocument = (SVGDocument) domImplementation.createDocument(svgNS, "svg", null);
+		DOMImplementation domImplementation = SVGDOMImplementation.getDOMImplementation();
+		String svgNS = "http://www.w3.org/2000/svg";
+		SVGDocument svgDocument = (SVGDocument) domImplementation.createDocument(svgNS, "svg", null);
 
-        SVGGraphics2D svgGraphics2D = new SVGGraphics2D(svgDocument);
-        svgGraphics2D.setSVGCanvasSize(bounds.getSize());
-        
-        component.paintAll(svgGraphics2D);
+		SVGGraphics2D svgGraphics2D = new SVGGraphics2D(svgDocument);
+		svgGraphics2D.setSVGCanvasSize(bounds.getSize());
 
-        if (new File(exportFile.getAbsolutePath() + ".temp").exists()) {
-            //new File(exportFile.getAbsolutePath() + ".temp").delete();
-        }
+		component.paintAll(svgGraphics2D);
 
-        OutputStream outputStream = new FileOutputStream(exportFile);
-        BufferedOutputStream bos = new BufferedOutputStream(outputStream);
-        Writer out = new OutputStreamWriter(bos, "UTF-8");
+		// removing clip paths
+		SVGSVGElement root = (SVGSVGElement) svgGraphics2D.getRoot();
+		Element defs = root.getElementById("defs1");
+		NodeList childs = defs.getChildNodes();
+		for (int i = childs.getLength() - 1; i >= 0; i--) {
+			defs.removeChild(childs.item(i));
+		}
 
-        svgGraphics2D.stream(out, true);
-        outputStream.flush();
-        outputStream.close();
-        out.close();
-        bos.close();
-    }
+		
+		if (new File(exportFile.getAbsolutePath() + ".temp").exists()) {
+			// new File(exportFile.getAbsolutePath() + ".temp").delete();
+		}
 
-    /**
-     * Export component to png format
-     * @param component Component
-     * @param bounds Rectangle
-     * @param exportFile Export file
-     * @throws IOException
-     */
-    private static void exportPNG(Component component, Rectangle bounds, File exportFile) throws IOException {
+		OutputStream outputStream = new FileOutputStream(exportFile);
+		BufferedOutputStream bos = new BufferedOutputStream(outputStream);
+		Writer out = new OutputStreamWriter(bos, "UTF-8");
 
-        BufferedImage bufferedImage = new BufferedImage(bounds.width, bounds.height, BufferedImage.TYPE_INT_RGB);
+		// dump root instead of svgGraphics2D because of XML manipulation
+		svgGraphics2D.stream(root, out, true, false);
+		outputStream.flush();
+		outputStream.close();
+		out.close();
+		bos.close();
+	}
 
-        Graphics g = bufferedImage.getGraphics();
+	/**
+	 * Export component to png format
+	 * 
+	 * @param component  Component
+	 * @param bounds     Rectangle
+	 * @param exportFile Export file
+	 * @throws IOException
+	 */
+	private static void exportPNG(Component component, Rectangle bounds, File exportFile) throws IOException {
 
-        component.paint(g);
+		BufferedImage bufferedImage = new BufferedImage(bounds.width, bounds.height, BufferedImage.TYPE_INT_RGB);
 
-        for (Iterator<ImageWriter> iw = ImageIO.getImageWritersByFormatName("png"); iw.hasNext();){
+		Graphics g = bufferedImage.getGraphics();
 
-            ImageWriter writer = iw.next();
-            ImageWriteParam writeParam = writer.getDefaultWriteParam();
+		component.paint(g);
 
-            ImageTypeSpecifier typeSpecifier = ImageTypeSpecifier.createFromBufferedImageType(BufferedImage.TYPE_INT_RGB);
-            IIOMetadata metadata = writer.getDefaultImageMetadata(typeSpecifier, writeParam);
+		for (Iterator<ImageWriter> iw = ImageIO.getImageWritersByFormatName("png"); iw.hasNext();) {
 
-            if (metadata.isReadOnly() || !metadata.isStandardMetadataFormatSupported()){
-                continue;
-            }
-            ImageOutputStream stream = null;
-            try {
+			ImageWriter writer = iw.next();
+			ImageWriteParam writeParam = writer.getDefaultWriteParam();
 
-                setDPI(metadata);
+			ImageTypeSpecifier typeSpecifier = ImageTypeSpecifier
+					.createFromBufferedImageType(BufferedImage.TYPE_INT_RGB);
+			IIOMetadata metadata = writer.getDefaultImageMetadata(typeSpecifier, writeParam);
 
-                stream = ImageIO.createImageOutputStream(exportFile);
-                writer.setOutput(stream);
-                writer.write(metadata, new IIOImage(bufferedImage, null, metadata), writeParam);
+			if (metadata.isReadOnly() || !metadata.isStandardMetadataFormatSupported()) {
+				continue;
+			}
+			ImageOutputStream stream = null;
+			try {
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                stream.close();
-            }
-            break;
-        }
-    }
+				setDPI(metadata);
 
-    /**
-     * Set DPI for png format pic
-     * @param metadata IIOMetadata
-     * @throws IIOInvalidTreeException
-     */
-    private static void setDPI(IIOMetadata metadata) throws IIOInvalidTreeException {
+				stream = ImageIO.createImageOutputStream(exportFile);
+				writer.setOutput(stream);
+				writer.write(metadata, new IIOImage(bufferedImage, null, metadata), writeParam);
 
-        int dpi = Toolkit.getDefaultToolkit().getScreenResolution();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				stream.close();
+			}
+			break;
+		}
+	}
 
-        double dotsPerMilli = 1000.0 * dpi / 10 / 2.54;
+	/**
+	 * Set DPI for png format pic
+	 * 
+	 * @param metadata IIOMetadata
+	 * @throws IIOInvalidTreeException
+	 */
+	private static void setDPI(IIOMetadata metadata) throws IIOInvalidTreeException {
 
-        IIOMetadataNode horizontalPixelSize = new IIOMetadataNode("HorizontalPixelSize");
-        horizontalPixelSize.setAttribute("value", String.valueOf(dotsPerMilli));
+		int dpi = Toolkit.getDefaultToolkit().getScreenResolution();
 
-        IIOMetadataNode verticalPixelSize = new IIOMetadataNode("VerticalPixelSize");
-        verticalPixelSize.setAttribute("value", String.valueOf(dotsPerMilli));
+		double dotsPerMilli = 1000.0 * dpi / 10 / 2.54;
 
-        IIOMetadataNode dimension = new IIOMetadataNode("Dimension");
-        dimension.appendChild(horizontalPixelSize);
-        dimension.appendChild(verticalPixelSize);
+		IIOMetadataNode horizontalPixelSize = new IIOMetadataNode("HorizontalPixelSize");
+		horizontalPixelSize.setAttribute("value", String.valueOf(dotsPerMilli));
 
-        IIOMetadataNode root = new IIOMetadataNode("javax_imageio_1.0");
-        root.appendChild(dimension);
+		IIOMetadataNode verticalPixelSize = new IIOMetadataNode("VerticalPixelSize");
+		verticalPixelSize.setAttribute("value", String.valueOf(dotsPerMilli));
 
-        metadata.mergeTree("javax_imageio_1.0", root);
+		IIOMetadataNode dimension = new IIOMetadataNode("Dimension");
+		dimension.appendChild(horizontalPixelSize);
+		dimension.appendChild(verticalPixelSize);
 
-    }
+		IIOMetadataNode root = new IIOMetadataNode("javax_imageio_1.0");
+		root.appendChild(dimension);
+
+		metadata.mergeTree("javax_imageio_1.0", root);
+
+	}
+
+	public static void removeAllClipPaths(SVGElement svg) {
+		/*
+		 * List<SVGElement> clipPathNodes = SVGUtil.getQuerySVGElements(svg,
+		 * "//svg:defs/svg:clipPath"); for (int i = 0; i < clipPathNodes.size(); i++) {
+		 * clipPathNodes.get(i).detach(); } Nodes clipPathElements =
+		 * svg.query("//*[@style[contains(.,'clip-path')]]"); for (int i = 0; i <
+		 * clipPathElements.size(); i++) { ((SVGElement)
+		 * clipPathElements.get(i)).setClipPath(null); }
+		 */
+	}
+
 }
