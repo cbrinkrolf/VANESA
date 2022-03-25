f954a3c9c446a72e05dfd31818b2ae78664a791f
cbrinkrolf <christoph.brinkrolf@uni-bielefeld.de>
2022-03-25 08:05:42 +0100

    Information window about node / edge types and counts
    - added information of true and logical nodes

--- src/gui/NodesEdgesTypesWindow.java
+++ src/gui/NodesEdgesTypesWindow.java
@@ -26,67 +26,95 @@ public class NodesEdgesTypesWindow {
 
 		int nodes = np.getNodeCount();
 		int edges = np.getEdgeCount();
-		
-
-		String instructions = "<html>"
-				+ tableStart
-				+ writeLine("Number of Nodes:", nodes + "")
-				+ writeLine("Number of Edges:", edges + "")
-				+ writeLine("<hr>Nodes", "");
-		
-		Map<String, Integer> nodesMap = getNodeTypes();
-		for(String type : nodesMap.keySet()){
-			instructions += writeLine(type+": ", nodesMap.get(type)+"");
+		int logicalNodes = 0;
+		for (BiologicalNodeAbstract bna : pw.getGraph().getAllVertices()) {
+			if (bna.hasRef()) {
+				logicalNodes++;
+			}
 		}
-		
-		
+
+		String instructions = "<html>" + tableStart + writeLine("", "all = ", "true +", "logical")
+				+ writeLine("Number of nodes:", nodes + "", nodes - logicalNodes + "", logicalNodes + "")
+				// + writeLine("Number of (true) nodes:", nodes - logicalNodes + "")
+				// + writeLine("Number of logical nodes:", logicalNodes + "")
+				+ writeLine("Number of edges:", edges + "") + writeLine("<hr>Nodes", "");
+
+		Map<String, Integer> nodesMap = getNodeTypes();
+		Map<String, Integer> nodesMapLogical = getNodeTypesLogical();
+
+		for (String type : nodesMap.keySet()) {
+			if (nodesMapLogical.containsKey(type)) {
+				instructions += writeLine(type + ": ", nodesMap.get(type) + "",
+						nodesMap.get(type) - nodesMapLogical.get(type) + "", nodesMapLogical.get(type) + "");
+			} else {
+				instructions += writeLine(type + ": ", nodesMap.get(type) + "");
+			}
+		}
+
 		Map<String, Integer> edgesMap = getEdgeTypes();
 		instructions += writeLine("<hr>Edges", "");
-		
-		for(String type : edgesMap.keySet()){
-			instructions += writeLine(type+": ", edgesMap.get(type)+"");
+
+		for (String type : edgesMap.keySet()) {
+			instructions += writeLine(type + ": ", edgesMap.get(type) + "");
 		}
 
 		instructions += tableEnd + "</html>";
 
 		// REENABLE
-		 JOptionPane.showMessageDialog(w.getFrame(), instructions,
-		 "Network Properties", JOptionPane.DEFAULT_OPTION);
-		
+		JOptionPane.showMessageDialog(w.getFrame(), instructions, "Network Properties", JOptionPane.DEFAULT_OPTION);
+
 	}
-	
-	private Map<String, Integer> getNodeTypes(){
+
+	private Map<String, Integer> getNodeTypes() {
 		Map<String, Integer> map = new HashMap<>();
 		String type;
-		for(BiologicalNodeAbstract bna : pw.getAllGraphNodes()){
+		for (BiologicalNodeAbstract bna : pw.getAllGraphNodes()) {
 			type = bna.getBiologicalElement();
-			if(!map.containsKey(type)){
+			if (!map.containsKey(type)) {
 				map.put(type, 0);
 			}
-			map.put(type, map.get(type)+1);
+			map.put(type, map.get(type) + 1);
 		}
 		return map;
 	}
-	
-	private Map<String, Integer> getEdgeTypes(){
+
+	private Map<String, Integer> getNodeTypesLogical() {
 		Map<String, Integer> map = new HashMap<>();
 		String type;
-		for(BiologicalEdgeAbstract bea : pw.getAllEdges()){
-			type = bea.getBiologicalElement();
-			if(!map.containsKey(type)){
-				map.put(type, 0);
+		for (BiologicalNodeAbstract bna : pw.getAllGraphNodes()) {
+			if (bna.hasRef()) {
+				type = bna.getBiologicalElement();
+				if (!map.containsKey(type)) {
+					map.put(type, 0);
+				}
+				map.put(type, map.get(type) + 1);
 			}
-			map.put(type, map.get(type)+1);
 		}
 		return map;
 	}
 
-	private String writeLine(String description, String Attribute) {
+	private Map<String, Integer> getEdgeTypes() {
+		Map<String, Integer> map = new HashMap<>();
+		String type;
+		for (BiologicalEdgeAbstract bea : pw.getAllEdges()) {
+			type = bea.getBiologicalElement();
+			if (!map.containsKey(type)) {
+				map.put(type, 0);
+			}
+			map.put(type, map.get(type) + 1);
+		}
+		return map;
+	}
 
-		return "<tr>"
-				+ "<th style=\"text-align: left;color:#666;text-transform:uppercase;\" scope=\"col\">"
-				+ description + "</th>"
-				+ "<td style=\"padding:5px;color:#888;\">" + Attribute
-				+ "</td></tr>";
+	private String writeLine(String description, String attribute1, String attribute2, String attribute3) {
+
+		return "<tr>" + "<th style=\"text-align: left;color:#666;text-transform:uppercase;\" scope=\"col\">"
+				+ description + "</th>" + "<td style=\"padding:5px;color:#888;\">" + attribute1 + "</td>"
+				+ "<td style=\"padding:5px;color:#888;\">" + attribute2 + "</td>"
+				+ "<td style=\"padding:5px;color:#888;\">" + attribute3 + "</td></tr>";
+	}
+
+	private String writeLine(String description, String attribute1) {
+		return this.writeLine(description, attribute1, "", "");
 	}
 }
