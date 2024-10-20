b46d207abd69517e38aaf6eb7353171101249c23
cbrinkrolf <christoph.brinkrolf@uni-bielefeld.de>
2024-10-20 12:38:09 -0400

    show errors during/after calling AsyncTaskExecutor
    exceptions were not passed, GUI remained blocked if execution failed

--- src/main/java/gui/AsyncTaskExecutor.java
+++ src/main/java/gui/AsyncTaskExecutor.java
@@ -3,11 +3,16 @@ package gui;
 import java.util.concurrent.CompletableFuture;
 
 public final class AsyncTaskExecutor {
-    public static void runUIBlocking(String label, Runnable body) {
-        MainWindow.getInstance().showProgressBar(label);
-        CompletableFuture.runAsync(() -> {
-            body.run();
-            MainWindow.getInstance().closeProgressBar();
-        });
-    }
+	public static void runUIBlocking(String label, Runnable body) {
+		MainWindow.getInstance().showProgressBar(label);
+		CompletableFuture.runAsync(() -> {
+			body.run();
+		}).exceptionally(t -> {
+			t.printStackTrace();
+			PopUpDialog.getInstance().show("Task Executor error.",
+					"Failed to run task: " + label + "\r\n" + t.getMessage());
+			return null;
+		});
+		MainWindow.getInstance().closeProgressBar();
+	}
 }
