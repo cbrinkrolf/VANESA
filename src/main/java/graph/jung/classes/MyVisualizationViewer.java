9ae31722947eb277d2637d120a13e3d51fa0d145
cbrinkrolf <christoph.brinkrolf@uni-bielefeld.de>
2024-10-21 21:48:40 -0400

    reduced number of created objects during rendering

--- src/main/java/graph/jung/classes/MyVisualizationViewer.java
+++ src/main/java/graph/jung/classes/MyVisualizationViewer.java
@@ -16,110 +16,114 @@ import edu.uci.ics.jung.visualization.VisualizationViewer;
 import graph.GraphContainer;
 
 public class MyVisualizationViewer<V, E> extends VisualizationViewer<V, E> {
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private final Pathway pw;
-    private Point2D mousePoint = new Point2D.Double(0, 0);
-    private int fpsCounter = 0;
-    private long start = 0;
-    private boolean printFPS = !true;
+	private Point2D mousePoint = new Point2D.Double(0, 0);
+	private int fpsCounter = 0;
+	private long start = 0;
+	private boolean printFPS = !true;
 
-    public MyVisualizationViewer(VisualizationModel<V, E> arg0, Dimension arg2, Pathway pw) {
-        super(arg0, arg2);
-        this.pw = pw;
-    }
+	private Font fontBold = new Font("default", Font.BOLD, 12);
+	private Font fontPlain = new Font("default", Font.PLAIN, 12);
 
-    // private long lastTimeCompartmentRendered = 0;
-    // private double scaleLastCompartmentRendered = 0;
+	public MyVisualizationViewer(VisualizationModel<V, E> arg0, Dimension arg2, Pathway pw) {
+		super(arg0, arg2);
+		this.pw = pw;
+	}
 
-    // private long[] relaxTimes = new long[5];
+	// private long lastTimeCompartmentRendered = 0;
+	// private double scaleLastCompartmentRendered = 0;
 
-    // private long[] paintTimes = new long[5];
+	// private long[] relaxTimes = new long[5];
 
-    // private int relaxIndex = 0;
+	// private long[] paintTimes = new long[5];
 
-    // private int paintIndex = 0;
+	// private int relaxIndex = 0;
 
-    // private double paintfps, relaxfps;
+	// private int paintIndex = 0;
 
-    /**
-     * a collection of user-implementable functions to render under the topology
-     * (before the graph is rendered)
-     */
-    // protected List<Paintable> preRenderers = new ArrayList<Paintable>();
+	// private double paintfps, relaxfps;
 
-    /**
-     * a collection of user-implementable functions to render over the topology (after the graph is rendered)
-     */
-    // protected List<Paintable> postRenderers = new ArrayList<Paintable>();
-    protected void renderGraph(Graphics2D g2d) {
-        super.renderGraph(g2d);
+	/**
+	 * a collection of user-implementable functions to render under the topology
+	 * (before the graph is rendered)
+	 */
+	// protected List<Paintable> preRenderers = new ArrayList<Paintable>();
 
-        g2d.setFont(new Font("default", Font.BOLD, 12));
-        g2d.setColor(Color.red);
+	/**
+	 * a collection of user-implementable functions to render over the topology
+	 * (after the graph is rendered)
+	 */
+	// protected List<Paintable> postRenderers = new ArrayList<Paintable>();
+	protected void renderGraph(Graphics2D g2d) {
+		super.renderGraph(g2d);
 
-        if (GraphContainer.getInstance().isPetriView()) {
-            g2d.drawString("P: " + pw.getPlaceCount() + " T: " + pw.getTransitionCount() + " Edges: " +
-                           pw.getGraph().getAllEdges().size(), 1, 11);
-        } else {
-            g2d.drawString(
-                    "Nodes: " + pw.getGraph().getAllVertices().size() + " Edges: " + pw.getGraph().getAllEdges().size(),
-                    1, 11);
-        }
-        g2d.drawString("Picked nodes: " + getPickedVertexState().getPicked().size(), 1, 23);
+		g2d.setFont(fontBold);
+		g2d.setColor(Color.red);
 
-        double scale = ((int) (getScale() * 100)) / 100.0;
-        g2d.drawString("Zoom: " + scale + "x", getWidth() - 75, 11);
+		if (GraphContainer.getInstance().isPetriView()) {
+			g2d.drawString("P: " + pw.getPlaceCount() + " T: " + pw.getTransitionCount() + " Edges: "
+					+ pw.getGraph().getAllEdges().size(), 1, 11);
+		} else {
+			g2d.drawString(
+					"Nodes: " + pw.getGraph().getAllVertices().size() + " Edges: " + pw.getGraph().getAllEdges().size(),
+					1, 11);
+		}
+		g2d.drawString("Picked nodes: " + getPickedVertexState().getPicked().size(), 1, 23);
 
-        drawMousePoint(g2d);
+		double scale = ((int) (getScale() * 100)) / 100.0;
+		g2d.drawString("Zoom: " + scale + "x", getWidth() - 75, 11);
 
-        // g2d.drawString("x", 580, 533);
-        // ContainerSingelton.getInstance().setPetriView(true);
-        if (printFPS) {
-            fpsCounter++;
-            printFPS();
-        }
-    }
+		drawMousePoint(g2d);
 
-    public List<VisualizationServer.Paintable> getPreRenderers() {
-        return super.preRenderers;
-    }
+		// g2d.drawString("x", 580, 533);
+		// ContainerSingelton.getInstance().setPetriView(true);
+		if (printFPS) {
+			fpsCounter++;
+			printFPS();
+		}
+	}
 
-    public List<VisualizationServer.Paintable> getPostRenderers() {
-        return super.postRenderers;
-    }
+	public List<VisualizationServer.Paintable> getPreRenderers() {
+		return super.preRenderers;
+	}
 
-    public Pathway getPathway() {
-        return pw;
-    }
+	public List<VisualizationServer.Paintable> getPostRenderers() {
+		return super.postRenderers;
+	}
 
-    public double getScale() {
-        double scaleV = getRenderContext().getMultiLayerTransformer().getTransformer(Layer.VIEW).getScale();
-        double scaleL = getRenderContext().getMultiLayerTransformer().getTransformer(Layer.LAYOUT).getScale();
-        return scaleV < 1 ? scaleV : scaleL;
-    }
+	public Pathway getPathway() {
+		return pw;
+	}
 
-    public void setMousePoint(Point2D point) {
-        this.mousePoint = point;
-        this.repaint(getWidth() - 75, 0, 75, 40);
-    }
+	public double getScale() {
+		double scaleV = getRenderContext().getMultiLayerTransformer().getTransformer(Layer.VIEW).getScale();
+		double scaleL = getRenderContext().getMultiLayerTransformer().getTransformer(Layer.LAYOUT).getScale();
+		return scaleV < 1 ? scaleV : scaleL;
+	}
 
-    private void drawMousePoint(Graphics2D g2d) {
-        g2d.setFont(new Font("default", Font.PLAIN, 12));
-        g2d.setColor(Color.black);
-        String text = (int) mousePoint.getX() + ", " + (int) mousePoint.getY();
-        Rectangle2D bounds = g2d.getFont().getStringBounds(text, g2d.getFontRenderContext());
-        g2d.drawString(text, (int) (getWidth() - bounds.getWidth()) - 10, 22);
-    }
+	public void setMousePoint(Point2D point) {
+		this.mousePoint = point;
+		this.repaint(getWidth() - 75, 0, 75, 40);
+	}
 
-    private void printFPS() {
-        long stop = System.currentTimeMillis();
-        if (stop - start > 1000) {
-            System.out.println("FPS: " + fpsCounter);
-            fpsCounter = 0;
-            start = stop;
-        }
-    }
+	private void drawMousePoint(Graphics2D g2d) {
+		g2d.setFont(fontPlain);
+		g2d.setColor(Color.black);
+		String text = (int) mousePoint.getX() + ", " + (int) mousePoint.getY();
+		Rectangle2D bounds = g2d.getFont().getStringBounds(text, g2d.getFontRenderContext());
+		g2d.drawString(text, (int) (getWidth() - bounds.getWidth()) - 10, 22);
+	}
+
+	private void printFPS() {
+		long stop = System.currentTimeMillis();
+		if (stop - start > 1000) {
+			System.out.println("FPS: " + fpsCounter);
+			fpsCounter = 0;
+			start = stop;
+		}
+	}
 }
