cbedf8a1731bc37fe0a3d2c24d332328843581e7
cbrinkrolf <christoph.brinkrolf@uni-bielefeld.de>
2025-02-04 18:52:20 -0500

    AsyncTaskkExecutor with the option to run code after completion

--- src/main/java/gui/AsyncTaskExecutor.java
+++ src/main/java/gui/AsyncTaskExecutor.java
@@ -16,4 +16,21 @@ public final class AsyncTaskExecutor {
 			return null;
 		});
 	}
+
+	public static void runUIBlocking(String label, Runnable body, Runnable complete) {
+		MainWindow.getInstance().showProgressBar(label);
+		CompletableFuture.runAsync(() -> {
+			body.run();
+			MainWindow.getInstance().closeProgressBar();
+		}).exceptionally(t -> {
+			t.printStackTrace();
+			MainWindow.getInstance().closeProgressBar();
+			PopUpDialog.getInstance().show("Task Executor error.",
+					"Failed to run task: " + label + "\r\n" + t.getMessage());
+			return null;
+		}).thenRun((() -> {
+			complete.run();
+			MainWindow.getInstance().closeProgressBar();
+		}));
+	}
 }
