361fbb62970abc5fe4e93650872e18c7155f6784
cbrinkrolf <christoph.brinkrolf@uni-bielefeld.de>
2025-02-04 19:04:57 -0500

    refactored AsyncTaskExecutor

--- src/main/java/gui/AsyncTaskExecutor.java
+++ src/main/java/gui/AsyncTaskExecutor.java
@@ -4,17 +4,7 @@ import java.util.concurrent.CompletableFuture;
 
 public final class AsyncTaskExecutor {
 	public static void runUIBlocking(String label, Runnable body) {
-		MainWindow.getInstance().showProgressBar(label);
-		CompletableFuture.runAsync(() -> {
-			body.run();
-			MainWindow.getInstance().closeProgressBar();
-		}).exceptionally(t -> {
-			t.printStackTrace();
-			MainWindow.getInstance().closeProgressBar();
-			PopUpDialog.getInstance().show("Task Executor error.",
-					"Failed to run task: " + label + "\r\n" + t.getMessage());
-			return null;
-		});
+		AsyncTaskExecutor.runUIBlocking(label, body, null);
 	}
 
 	public static void runUIBlocking(String label, Runnable body, Runnable complete) {
@@ -29,7 +19,9 @@ public final class AsyncTaskExecutor {
 					"Failed to run task: " + label + "\r\n" + t.getMessage());
 			return null;
 		}).thenRun((() -> {
-			complete.run();
+			if (complete != null) {
+				complete.run();
+			}
 			MainWindow.getInstance().closeProgressBar();
 		}));
 	}
