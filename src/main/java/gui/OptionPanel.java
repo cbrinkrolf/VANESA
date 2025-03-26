9e7439cf6892a23a5deb019b66d1721a74b764f8
Marcel Friedrichs <astror.enales@gmail.com>
2025-03-26 06:23:04 +0100

    Removed old and unused building blocks panel

--- src/main/java/gui/OptionPanel.java
+++ src/main/java/gui/OptionPanel.java
@@ -5,9 +5,7 @@ import javax.swing.*;
 import org.jdesktop.swingx.JXTaskPane;
 import org.jdesktop.swingx.JXTaskPaneContainer;
 
-import configurations.Workspace;
 import database.gui.DatabaseWindow;
-import gui.optionPanelWindows.BuildingBlocks;
 import gui.optionPanelWindows.ElementTree;
 import gui.optionPanelWindows.ElementWindow;
 import gui.optionPanelWindows.GraphAlgorithmsWindow;
@@ -23,7 +21,6 @@ public class OptionPanel {
 	private final JPanel panel;
 	private final ElementTree tree;
 	private final SimulationResultsPlot simResWindow;
-	private final BuildingBlocks bb;
 	private final SatelliteWindow satelliteWindow;
 	private final ElementWindow elementWindow;
 	private final ProjectWindow projectWindow;
@@ -35,20 +32,9 @@ public class OptionPanel {
 	private final JXTaskPane databaseSearch;
 
 	private boolean updatePanels = true;
-	private int lastFullWidth = -1;
-
-	private final JXTaskPaneContainer taskPaneContainer = new JXTaskPaneContainer();
-
-	// private GraphAlignmentOptionTab alignmentOptions;
-	// private JXTaskPane alignment;
-	// private HashMap<String, GraphAlignmentOptionTab> alignmentTabs = new
-	// HashMap<String, GraphAlignmentOptionTab>();
-
-	public JXTaskPaneContainer getTaskPaneContainer() {
-		return taskPaneContainer;
-	}
 
 	public OptionPanel() {
+		final JXTaskPaneContainer taskPaneContainer = new JXTaskPaneContainer();
 		taskPaneContainer.setLayout(new MigLayout("insets 0, wrap, fill"));
 		taskPaneContainer.setBorder(BorderFactory.createEmptyBorder(0, 0, 6, 0));
 
@@ -81,22 +67,8 @@ public class OptionPanel {
 		final JXTaskPane pathwayProperties = createCollapsiblePane("Pathway Properties", null, true,
 				pathwayPropertiesWindow);
 
-		bb = new BuildingBlocks();
-		final JXTaskPane bbProperties = createCollapsiblePane("Building Blocks", null, true, bb.getPanel());
-
 		projectWindow = new ProjectWindow();
 		final JXTaskPane project = createCollapsiblePane("Project Description", null, true, projectWindow.getPanel());
-
-		// alignment = new JXTaskPane("Graph Alignment");
-		// alignment.setCollapsed(false);
-		// alignment.setVisible(false);
-
-		/*
-		 * petriNet = new JXTaskPane("Petri Net Properties");
-		 * petriNet.setCollapsed(false); petriNet.setVisible(true); petriNetProperties =
-		 * new PetriNetProperties(); petriNet.add(petriNetProperties.getPanel());
-		 * taskPaneContainer.add(petriNet);
-		 */
 
 		taskPaneContainer.add(databaseSearch, "growx");
 		taskPaneContainer.add(simResView, "growx");
@@ -105,17 +77,8 @@ public class OptionPanel {
 		taskPaneContainer.add(theory, "growx");
 		taskPaneContainer.add(satellite, "growx");
 		taskPaneContainer.add(elements, "growx");
-		taskPaneContainer.add(bbProperties, "growx");
 		taskPaneContainer.add(pathways, "growx");
-		// taskPaneContainer.add(dbProperties);
-		if (Workspace.getCurrentSettings().isDeveloperMode()) {
-			// taskPaneContainer.add(heatgraphProperties, "growx");
-			taskPaneContainer.add(project, "growx");
-			// taskPaneContainer.add(alignment);
-		} else {
-			taskPaneContainer.add(project, "growx");
-			// taskPaneContainer.add(pcpview);
-		}
+		taskPaneContainer.add(project, "growx");
 
 		taskPaneContainer.setScrollableTracksViewportHeight(true);
 		panel = new JPanel(new MigLayout("insets 0, fill"));
@@ -146,7 +109,6 @@ public class OptionPanel {
 
 	public void removeAllElements() {
 		tree.removeTree();
-		bb.removeTree();
 		pathwayTree.removeTree();
 		satelliteWindow.removeAllElements();
 		simResWindow.removeAllElements();
@@ -161,7 +123,6 @@ public class OptionPanel {
 	}
 
 	public void updatePanel(String element) {
-		// System.out.println("element: " + element);
 		if (!updatePanels)
 			return;
 		switch (element) {
@@ -172,7 +133,6 @@ public class OptionPanel {
 			satelliteWindow.revalidateSatelliteView();
 			break;
 		case "simulation":
-			// PCPWindow.initGraphs();
 			simResWindow.revalidateView();
 			break;
 		case "element":
@@ -189,9 +149,6 @@ public class OptionPanel {
 			break;
 		case "initSimulation":
 			simResWindow.initGraphs();
-			break;
-		case "bb":
-			// bb.revalidateView();
 			break;
 		case "pathwayProperties":
 			pathwayPropertiesWindow.revalidateView();
@@ -226,18 +183,5 @@ public class OptionPanel {
 
 	public void addSimulationResults() {
 		simResWindow.addSimulationResults();
-	}
-
-	public int getFullWidth() {
-		if (scrollPane.getVerticalScrollBar().isShowing()) {
-			lastFullWidth = (int) scrollPane.getPreferredSize().getWidth() + UIManager.getInt("ScrollBar.width");
-		} else {
-			lastFullWidth = (int) scrollPane.getPreferredSize().getWidth();
-		}
-		return lastFullWidth;
-	}
-
-	public int getLastFullWidth() {
-		return lastFullWidth;
 	}
 }
